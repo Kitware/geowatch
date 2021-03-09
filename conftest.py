b2b1a176e4f75b363bd7c14c47b7a14585f11914
joncrall <erotemic@gmail.com>
2021-03-09 14:15:14 -0500

    Add conftest to allow specifying cli args

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest  # NOQA
+
+
+def pytest_addoption(parser):
+    # Allow CLI flags to be passed in as an option on sys.argv
+    # Thes are used by xdoctest, pytest currently does nothing with them
+    parser.addoption("--network", action="store_true")
+    parser.addoption("--slow", action="store_true")
