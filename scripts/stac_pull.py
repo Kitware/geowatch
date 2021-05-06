3f1a004c38c3acab9299b302f15f388464cba641
Jacob DeRosa <jacob.derosa@kitware.com>
2021-05-06 14:29:43 -0400

    added commline features

--- scripts/stac_pull.py
+++ scripts/stac_pull.py
@@ -6,53 +6,35 @@ import mgrs
 import argparse
 import sys
 
-def file_to_date(file):
-    year = '20' + file[5:7]
-    month_dict = {
-        'JAN': '01',
-        'FEB': '02', 
-        'MAR': '03', 
-        'APR': '04',
-        'MAY': '05', 
-        'JUN': '06', 
-        'JUL': '07',
-        'AUG': '08', 
-        'SEP': '09', 
-        'OCT': '10', 
-        'NOV': '11', 
-        'DEC': '12'
-    }
-    month = month_dict[file[2:5]]
-    day = file[0:2]
-    return os.path.join(year, month, day)
+def pull(out_dir, AOI, daterange, api_key, endpoint, jsondump):  
+    '''args:  out_dir = path/to/output/image/directory  
+              AOI = [xmin, ymin, xmax, ymax]  
+              datetime = ['year_min', 'year_max']
 
-def pull(out_dir, AOI, datetime):
-    #args:  out_dir = path/to/output/image/directory  bbox = [xmin, ymin, xmax, ymax]  datetime = ['year_min', 'year_max']
-    catalog = Client.open('https://api.smart-stac.com/', headers={"x-api-key": os.environ['STAC_API_KEY']})
-    catalog.to_dict()
+       returns: retrieved catalog as a dict, ready to be dumped as a JSON
+    '''
+    catalog = Client.open(endpoint, headers={"x-api-key": api_key})
     children = catalog.get_child_links()
-    m = mgrs.MGRS()
-    zone = m.toMGRS(AOI[3], AOI[0])
     for child in children:
         print(child)
-    search = catalog.search(collections=['worldview-nitf'], bbox=AOI, datetime = datetime)
+    search = catalog.search(collections=['worldview-nitf'], bbox=AOI, datetime = daterange)
     print(f"{search.matched()} Items found")
     for item in search.items():
-        print("Image: " + item.id)
-        id = item.id
-        s3 = os.path.join('s3://smart-imagery/worldview-nitf', zone[0:2], zone[2], zone[3:5], file_to_date(id), id[16:])
-        print("Downloading from " + s3)
-        cmd_args = ['aws', 's3', '--profile', 'iarpa', 'cp', os.path.join(s3, id+'.NTF'), os.path.join(out_dir, id+'.NTF')]
+        s3 = item.assets['data'].get_absolute_href()
+        cmd_args = ['aws', 's3', '--profile', 'iarpa', 'cp', s3, os.path.join(out_dir, item.id+'.NTF')]
         subprocess.run(cmd_args, check=True)
-
-    print('DONE')
+    return catalog.to_dict()
 
 def main(args):
     parser = argparse.ArgumentParser(
         description="Pull WorldView imagery from STAC")
-    parser.add_argument("--AOI",
-                        help="WATCH site to pull images for")
+    parser.add_argument("--AOI", help="Site to pull images for")
     parser.add_argument("--out_dir", help="Output directory")
+    parser.add_argument("--endpoint", help="STAC API endpoint")
+    parser.add_argument("--api_key", help="STAC API key", default=os.environ['STAC_API_KEY'])
+    parser.add_argument("--bbox", help="User-defined AOI as a JSON string", default=None)
+    parser.add_argument("--daterange", help="Date range to pull images for", default='["2014", "2018"]')
+    parser.add_argument("--jsondump", help="Dump the retrieved STAC catalog as a JSON", action="store_true")
     args = parser.parse_args(args)
     aoi_dict = {
         "KR-Pyeongchang": [128.662489, 37.659517, 128.676673, 37.664560],
@@ -61,12 +43,24 @@ def main(args):
         "BR-Rio-0277": [-43.342788, -22.960878, -43.336003, -22.954186],
         "BR-Rio-0270": [-43.438075, -22.999946, -43.432115, -23.003319]
     }
-    datetime = ['2014', '2018']
-
-    pull(args.out_dir, aoi_dict[args.AOI], datetime)
+    if args.bbox:
+        aoi_dict[args.AOI] = json.loads(args.bbox)
+    daterange = json.loads(args.daterange)
+    search = pull(args.out_dir, aoi_dict[args.AOI], daterange, args.api_key, args.endpoint, args.jsondump)
+    if args.jsondump:
+        with open(os.path.join(args.out_dir, args.AOI + '_catalog.json'), 'w') as f:
+            json.dump(search, f)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
     
-    #Usage: STAC_API_KEY=<api key> python3 stac_pull.py --AOI <WATCH site> --out_dir <output directory>
-    #This assumes that the user has properly configured AWS keys to access s3
+    '''
+    Usage: python3 stac_pull.py --AOI <WATCH site> --out_dir <output directory> --endpoint <STAC endpoint>
+                                --api_key <STAC API key> --bbox '[<xmin>, <ymin>, <xmax>, <ymax>]'
+                                --daterange '["<start date>", "<end date>"]' --jsondump (optional)
+
+    bbox and daterange arguments should be JSON strings.
+    Start and end dates in daterange should be in UTC format.
+    If a bbox is provided, the user should provide a name for it via the AOI argument.
+    This assumes that the user has properly configured AWS keys to access s3.
+    '''
