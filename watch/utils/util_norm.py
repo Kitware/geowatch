9f8667e0577192afb0232a11fccc327a1e762ef5
Matthew <matthew.bernstein@kitware.com>
2021-05-20 18:34:22 -0400

    split out utility functions

--- watch/utils/util_norm.py
+++ watch/utils/util_norm.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import kwimage as ki
+import rasterio
+from tempenv import TemporaryEnvironment
 
 
 def normalize_intensity(imdata, return_info=False, nodata=None):
@@ -105,3 +108,43 @@ def normalize_intensity(imdata, return_info=False, nodata=None):
         return result, info
     else:
         return result
+
+
+def thumbnail(in_path, sat=None, max_dim=1000):
+    '''
+    Create a small, true-color [1] thumbnail from a satellite image.
+
+    Args:
+        in_path: path to a S2, L7, or L8 scene readable by gdal
+        sat: if None, assume this is a true color (3-channel) or panchromatic (1-channel) image.
+            Otherwise, grab 3 color bands for 'WV', 'S2', 'L7', or 'L8'.
+        max_dim: max dimension of the thumbnail
+
+    Returns:
+        uint8, 3-channel RGB image content.
+
+    References:
+        [1] https://github.com/sentinel-hub/custom-scripts/tree/master
+    '''
+    # workaround for
+    # https://rasterio.readthedocs.io/en/latest/faq.html#why-can-t-rasterio-find-proj-db-rasterio-from-pypi-versions-1-2-0
+    with TemporaryEnvironment({'PROJ_LIB': None}):
+        with rasterio.open(in_path) as f:
+            if sat == None:
+                assert f.indexes.issubset((1,), (3,)), f'{in_path} is not PAN or TCI.'
+                bands = f.read()
+                bands = ki.atleast_3channels(bands)
+            elif sat == 'S2':
+                bands = np.stack([f.read(4), f.read(3), f.read(2)], axis=-1)
+            elif sat == 'L8':
+                bands = np.stack([f.read(4), f.read(3), f.read(2)], axis=-1)
+            elif sat == 'L7':
+                bands = np.stack([f.read(3), f.read(2), f.read(1)], axis=-1)
+            elif sat == 'WV':
+                bands = np.stack([f.read(5), f.read(3), f.read(2)], axis=-1)
+
+    bands = normalize_intensity(bands, nodata=0)
+    bands = ki.ensure_uint8(bands)
+    bands = ki.imresize(bands, max_dim=max_dim)
+    return bands
+
---
+++ watch/utils/util_rgdc.py
@@ -0,0 +1,87 @@
+import numpy as np
+from dateutils.parser import isoparse
+
+
+def group_tiles(query, timediff_sec=300):
+    '''
+    Given a RGDC search query result, nest results so that scenes
+    that can be merged are grouped together
+
+    TODO when RGDC STAC endpoint is available, calculate overlap with AOI here
+  
+    Args:
+        query: output of rgdc.search
+        timediff_sec: max allowed time between adjacent scenes.
+            Should nominally be 23 seconds for Landsat 7 and 8. Some buffer is included.
+            TODO what is it for Sentinel?
+    Example:
+        >>> # xdoctest: +SKIP   
+        >>> from rgdc import Rgdc
+        >>> client = Rgdc('username', 'password')
+        >>> query = (client.search(**kwargs, instrumentation='ETM') +
+        >>>          client.search(**kwargs, instrumentation='OLI_TIRS'))
+        >>> # query == [scene1, scene2, scene3, scene4]
+        >>> query = group_tiles(query)
+        >>> # query == [[scene1], [scene2, scene3], [scene4]]
+    '''
+    # ensure we're only working with one satellite at a time
+    query = sorted(query, key=lambda q: isoparse(q['acquisition_date']))
+    sensors, ixs = np.unique([q['instrumentation'] for q in query],
+                             return_index=True)
+    assert set(sensors).issubset({'ETM', 'OLI_TIRS', 'S2A', 'S2B'}), f'unsupported sensor in {sensors}'
+    to_process = np.split(query, ixs[1:])
+
+    def _path_row(q):
+        '''
+        Returns LS path and row as strings. eg ('115', '034')
+        '''
+        path_row = q['subentry_name'].split('_')[2]
+        path, row = path_row[:3], path_row[3:]
+        return path, row
+
+    result = []
+    for sensor, query in zip(sensors, to_process):
+        # extract and split by datetimes
+        dts = [isoparse(q['acquisition_date']) for q in query]
+        diffs = np.diff([dt.timestamp() for dt in dts],
+                        prepend=dts[0].timestamp())
+        ixs = np.where(diffs > timediff_sec)[0]
+
+        for split in np.split(query, ixs):
+            # for Landsat, an additional check:
+            # each split should consist of adjacent rows in the same path.
+            if sensor in {'ETM', 'OLI_TIRS'}:
+                path_rows = [_path_row(s) for s in split]
+                paths, rows = zip(*path_rows)
+                assert len(np.unique(paths)) == 1
+                assert len(np.unique(rows)) == len(split)
+
+            result.append(split)
+
+    return result
+
+
+def bands_landsat(entry):
+    '''
+    Get only the band files from a Landsat RasterMetaEntry downloaded from RGD.
+
+    Args:
+        entry: RasterMetaEntry, the output type of rgdc.download_raster
+
+    Returns:
+        list of pathlib paths to band files
+    '''
+    return [str(p) for p in entry.images if 'BQA' not in p.stem]
+
+
+def bands_sentinel2(entry):
+    '''
+    Get only the band files from a Sentinel-2 RasterMetaEntry downloaded from RGD.
+
+    Args:
+        entry: RasterMetaEntry, the output type of rgdc.download_raster
+
+    Returns:
+        list of pathlib paths to band files
+    '''
+    return [str(p) for p in entry.images if p.match('*_B*.jp2')]
