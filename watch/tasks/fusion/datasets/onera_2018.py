563df93a1e347a3693a62beddde9f292054cf76c
Connor Greenwell <connor.greenwell@kwintern-1811.khq.kitware.com>
2021-06-01 12:25:45 -0400

    sgd voting model

--- watch/tasks/fusion/datasets/onera_2018.py
+++ watch/tasks/fusion/datasets/onera_2018.py
@@ -125,3 +125,117 @@ class OneraDataset(data.Dataset):
         }
         
         return example
+
+
+class SimpleDataset(data.Dataset):
+    # TODO: add torchvision.transforms or albumentations
+    def __init__(self, sampler, sample_shape, channels=None, mode="fit"):
+        self.sampler = sampler
+        self.sample_shape = sample_shape
+        self.channels = channels
+        self.mode = mode
+        
+        full_sample_grid = self.sampler.new_sample_grid("video_detection", self.sample_shape)
+        self.sample_grid = list(it.chain(
+            full_sample_grid["positives"], 
+            full_sample_grid["negatives"],
+        ))
+        
+        example_to_query = self.__getitem__(0)
+        if self.mode == "predict":
+            self.num_channels = example_to_query.shape[1]
+        else:
+            self.num_channels = example_to_query["images"].shape[1]
+        
+    def compute_stats(self, num_examples=1):
+        
+        # get some samples, reshape/flatten so that channels lead
+        images = torch.stack([
+            self.__getitem__(idx)["images"]
+            for idx in range(num_examples)
+        ], dim=0).numpy()
+        channel_images = einops.rearrange(
+            images,
+            "b t c h w -> c (b t h w)",
+        )
+        
+        # dump outliers
+        cmin, cmax = np.percentile(channel_images, [2, 98], axis=1)
+        channel_images[channel_images < cmin[:,None]] = np.nan
+        channel_images[channel_images > cmax[:,None]] = np.nan
+        
+        # compute stats
+        return channel_images.mean(axis=1), channel_images.std(axis=1)
+    
+    def __len__(self):
+        return len(self.sample_grid)
+    
+    def __getitem__(self, idx):
+        
+        # get positive sample definition
+        tr = self.sample_grid[idx]
+        if self.channels:
+            tr["channels"] = self.channels
+        
+        # collect sample
+        sample = self.sampler.load_sample(tr)
+
+        # Access the sampled image and annotation data
+        raw_frame_list = sample['im']
+        raw_det_list = sample['annots']['frame_dets']
+
+        # Break data down on a per-frame basis so we can apply image-based
+        # augmentations.
+        frame_ims = []
+        frame_masks = []
+        for frame, dets in zip(raw_frame_list, raw_det_list):
+            frame = frame.astype(np.float32)
+            input_dsize = self.sample_shape[-2:]
+            
+            input_dsize = (
+                real if (nominal is None) else nominal
+                for nominal, real in zip(input_dsize, frame.shape)
+            )
+
+            # Resize the sampled window to the target space for the network
+            frame, info = kwimage.imresize(frame, dsize=input_dsize,
+                                           interpolation='linear',
+                                           return_info=True)
+            # Remember to apply any transform to the dets as well
+            dets = dets.scale(info['scale'])
+            dets = dets.translate(info['offset'])
+
+            frame_mask = np.full(frame.shape[0:2], dtype=np.int32, fill_value=-1)
+            ann_polys = dets.data['segmentations'].to_polygon_list()
+            ann_aids = dets.data['aids']
+            ann_cids = dets.data['cids']
+
+            for poly, aid, cid in zip(ann_polys, ann_aids, ann_cids):
+                cidx = self.sampler.classes.id_to_idx[cid]
+                poly.fill(frame_mask, value=cidx)
+
+            # ensure channel dim is not squeezed
+            frame = kwarray.atleast_nd(frame, 3)
+
+            frame_masks.append(frame_mask)
+            frame_ims.append(frame)
+            
+        # stack along temporal axis
+        frame_masks = np.stack(frame_masks, axis=0)
+        frame_ims = np.stack(frame_ims, axis=0)
+        
+        # rearrange image axes for pytorch
+        frame_ims = einops.rearrange(frame_ims, "t h w c -> t c h w")
+        
+        # catch nans
+        frame_ims[np.isnan(frame_ims)] = -1.
+        
+        if self.mode == "predict":
+            return torch.from_numpy(frame_ims).detach()
+
+        example = {
+            "images": torch.from_numpy(frame_ims).detach(),
+            "labels": torch.from_numpy(frame_masks).detach().int()+1,
+        }
+        
+        return example
---
+++ watch/tasks/fusion/methods/voting.py
@@ -0,0 +1,94 @@
+import torch
+from torch import nn
+import pytorch_lightning as pl
+import torch_optimizer as optim
+from torch.optim import lr_scheduler
+import einops
+import torchmetrics as metrics
+
+from models import unet_blur
+
+class VotingModel(pl.LightningModule):
+    def __init__(self, input_dim, learning_rate=1e-5, weight_decay=1e-5, pos_weight=1.):
+        super().__init__()
+        self.save_hyperparameters()
+        
+        # simple feature extraction model
+        self.model = nn.Conv2d(input_dim, 1, 1, bias=False)
+        
+        # criterion and metrics
+        self.criterion = nn.BCEWithLogitsLoss(pos_weight=torch.ones(1)*pos_weight)
+        self.metrics = nn.ModuleDict({
+            "acc": metrics.Accuracy(),
+            "f1": metrics.F1(),
+        })
+        
+    def forward(self, x):
+        B = x.shape[0]
+        x = einops.rearrange(x, "b t c h w -> (b t) c h w")
+        y = self.model(x)
+        y = einops.rearrange(y, "(b t) 1 h w -> b t h w", b=B)
+        return y
+        
+    def training_step(self, batch, batch_idx=None):
+        images, changes = batch["images"], batch["labels"]
+        
+        # compute predicted and target change masks
+        distances = self(images)
+        
+        # compute metrics
+        for key, metric in self.metrics.items():
+            self.log(key, metric(torch.sigmoid(distances), changes), prog_bar=True)
+        
+        # compute criterion
+        loss = self.criterion(distances, changes.float())
+        return loss
+    
+    def validation_step(self, batch, batch_idx=None):
+        images, changes = batch["images"], batch["labels"]
+        
+        # compute predicted and target change masks
+        distances = self(images)
+                
+        # compute metrics
+        for key, metric in self.metrics.items():
+            self.log("val_"+key, metric(torch.sigmoid(distances), changes), prog_bar=True)
+        
+        # compute loss
+        loss = self.criterion(distances, changes.float())
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+    
+    def test_step(self, batch, batch_idx=None):
+        images, changes = batch["images"], batch["labels"]
+        
+        # compute predicted and target change masks
+        distances = self(images)
+                
+        # compute metrics
+        for key, metric in self.metrics.items():
+            self.log("test_"+key, metric(torch.sigmoid(distances), changes), prog_bar=True)
+        
+        # compute loss
+        loss = self.criterion(distances, changes.float())
+        self.log("test_loss", loss, prog_bar=True)
+        return loss
+    
+    def configure_optimizers(self):
+        optimizer = optim.RAdam(
+                self.model.parameters(), 
+                lr=self.hparams.learning_rate, 
+                weight_decay=self.hparams.weight_decay,
+                betas=(0.9, 0.99),
+            )
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.trainer.max_epochs)
+        return [optimizer], [scheduler]
+    
+    
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("VotingModel")
+        parser.add_argument("--learning_rate", default=1e-5, type=float)
+        parser.add_argument("--weight_decay", default=1e-5, type=float)
+        parser.add_argument("--pos_weight", default=1.0, type=float)
+        return parent_parser
