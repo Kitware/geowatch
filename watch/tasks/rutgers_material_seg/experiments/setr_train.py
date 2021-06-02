347e72dab4f21490114cd90373f0dd3e373f9931
periakiva <periha@gmail.com>
2021-06-02 17:57:25 -0400

    SETR benchmark with SMART dataset

---
+++ watch/tasks/rutgers_material_seg/experiments/setr_train.py
@@ -0,0 +1,16 @@
+from watch.tasks.rutgers_material_seg.models.transformer_seg import SETRModel
+import torch 
+
+if __name__ == "__main__":
+    net = SETRModel(patch_size=(32, 32), 
+                    in_channels=3, 
+                    out_channels=1, 
+                    hidden_size=1024, 
+                    num_hidden_layers=8, 
+                    num_attention_heads=16, 
+                    decode_features=[512, 256, 128, 64])
+    t1 = torch.rand(1, 3, 256, 256)
+    print("input: " + str(t1.shape))
+    
+    # print(net)
+    print("output: " + str(net(t1).shape))
