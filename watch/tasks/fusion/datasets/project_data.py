b11bdfb636856f4530cce1e5ad94b2e6aea7ba8b
Connor Greenwell <connor.greenwell@kwintern-1811.khq.kitware.com>
2021-06-10 13:46:57 -0400

    create lightningDataModules for onera and project data (s2 only)

--- watch/tasks/fusion/datasets/project_data.py
+++ watch/tasks/fusion/datasets/project_data.py
@@ -8,7 +8,7 @@ from torch.utils import data
 from datasets import common
 import utils
 
-class OneraCD_2018(pl.LightningDataModule):
+class Drop0AlignMSI_S2(pl.LightningDataModule):
     def __init__(
         self, 
         train_kwcoco_path=None, 
@@ -17,7 +17,7 @@ class OneraCD_2018(pl.LightningDataModule):
         chip_size=128,
         time_overlap=0,
         chip_overlap=0.1,
-        channels='B01|B02|B03|B04|B05|B06|B07|B08|B09|B10|B11|B12|B8A',
+        channels='costal|blue|green|red|B05|B06|B07|nir|B09|cirrus|swir16|swir22|B8A',
         valid_pct=0.1,
         batch_size=4,
         num_workers=4,
@@ -34,11 +34,69 @@ class OneraCD_2018(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
         
+    def preprocess_ds(self, project_ds):
+        project_ds.remove_images([
+            image
+            for image in project_ds.dataset["images"]
+            if "S2-TrueColor" not in image["sensor_candidates"]
+        ])
+        project_ds.remove_videos([2,3])
+
+        for image in project_ds.dataset["images"]:
+            image["warp_img_to_vid"] = {
+                'type': 'affine',
+                'matrix': [
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                ]}
+
+            for band in image["auxiliary"]:
+                band["warp_aux_to_img"]["type"] = "affine"
+
+        for video in project_ds.dataset["videos"]:
+            video_id = video["id"]
+
+            video["width"] = max([
+                    image["width"]
+                    for image in project_ds.dataset["images"]
+                    if image["video_id"] == video["id"]
+                ])
+            video["height"] = max([
+                    image["height"]
+                    for image in project_ds.dataset["images"]
+                    if image["video_id"] == video["id"]
+                ])
+            video["num_frames"] = len([
+                    image
+                    for image in project_ds.dataset["images"]
+                    if image["video_id"] == video["id"]
+                ])
+            video["available_channels"] = list(set(it.chain.from_iterable([
+                    [
+                        band["channels"]
+                        for band in image["auxiliary"]
+                    ]
+                    for image in project_ds.dataset["images"]
+                    if image["video_id"] == video_id
+                ])))
+            video["warp_wld_to_vid"] = {
+                'type': 'affine',
+                'matrix': [
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                ]}
+
+        project_ds.validate()
+        return project_ds
+        
     def setup(self, stage):
         transform = utils.Lambda(lambda x: x/2000.)
         
         if stage == "fit" or stage is None:
             kwcoco_ds = kwcoco.CocoDataset(self.train_kwcoco_path)
+            kwcoco_ds = self.preprocess_ds(kwcoco_ds)
             kwcoco_sampler = ndsampler.CocoSampler(kwcoco_ds)
             train_val_ds = common.VideoDataset(
                 kwcoco_sampler,
@@ -59,6 +117,7 @@ class OneraCD_2018(pl.LightningDataModule):
             
         if stage == "test" or stage is None:
             kwcoco_ds = kwcoco.CocoDataset(self.test_kwcoco_path)
+            kwcoco_ds = self.preprocess_ds(kwcoco_ds)
             kwcoco_sampler = ndsampler.CocoSampler(kwcoco_ds)
             test_ds = common.VideoDataset(
                 kwcoco_sampler,
