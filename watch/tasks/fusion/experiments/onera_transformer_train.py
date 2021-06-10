dcb2d7b84675d500a833a880d014e62b8f77d6b2
Connor Greenwell <connor.greenwell@kwintern-1811.khq.kitware.com>
2021-06-10 17:01:44 -0400

    scripts for training a bank of unet/transformer change detectors on onera/drop0

---
+++ watch/tasks/fusion/experiments/onera_transformer_train.py
@@ -0,0 +1,47 @@
+import fit
+import pathlib
+import pytorch_lightning as pl
+
+from methods import baseline
+from datasets import onera_2018
+import utils
+
+
+channel_combos = {
+    "all": "<all>",
+    "uv": "B01",
+    "bgr": "B02|B03|B04",
+    "vnir": "B05|B06|B07|B08|B8A",
+    "swir": "B09|B10|B11|B12",
+}
+
+if __name__ == "__main__":
+    
+    from types import SimpleNamespace
+    
+    args = SimpleNamespace(
+        dataset="Onera_2018",
+        method="TransformerChangeDetector",
+        
+        # dataset params
+        train_kwcoco_path="~/Projects/smart_watch_dvc/extern/onera_2018/onera_train.kwcoco.json",
+        batch_size=64,
+        num_workers=8,
+        
+        # model params
+        window_size=8,
+        embedding_dim=256,
+        n_layers=8,
+        learning_rate=1e-3,
+        weight_decay=1e-5,
+        pos_weight=5.0,
+        
+        # trainer params
+        gpus=1,
+        max_epochs=200,
+    )
+    
+    for key, channels in channel_combos.items():
+        args.channels = channels
+        args.default_root_dir = f"experiments/onera/tf/{key}"
+        fit.main(args)
