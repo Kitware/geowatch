f85f158371417ee5dab497d1b89bb8a21a83628c
David Joy <david.joy@kitware.com>
2021-07-12 18:32:28 -0400

    Fixes for stac_egress and moved to standalone tool

--- atk/image_conditioning/algorithms/kitware/stac_egress/main.py
+++ atk/image_conditioning/algorithms/kitware/stac_egress/main.py
@@ -1,10 +1,9 @@
-import os
-import json
-import subprocess
-import tempfile
 import copy
 
+import pystac
 from algorithm_toolkit import Algorithm, AlgorithmChain
+
+from watch.tools.stac_egress import stac_egress
 
 
 class Main(Algorithm):
@@ -13,52 +12,16 @@ class Main(Algorithm):
         cl = self.cl  # type: AlgorithmChain.ChainLedger
         params = self.params  # type: dict
 
-        stac_catalog = copy.deepcopy(params['stac_catalog'])
+        stac_catalog_dict = copy.deepcopy(params['stac_catalog'])
+        stac_catalog = pystac.Catalog.from_dict(stac_catalog_dict)
 
-        # Upload each feature's assets from the STAC catalog
-        for feature in stac_catalog.get('features', ()):
-            feature_id = feature['id']
-            local_path = feature['assets']['data']['href']
+        dry_run = 'dry_run' in params and params['dry_run'] == 1
 
-            local_basename = os.path.basename(local_path)
+        output_stac_catalog = stac_egress(stac_catalog,
+                                          params['s3_bucket'],
+                                          dry_run)
 
-            upload_path = os.path.join(
-                params['s3_bucket'], feature_id, local_basename)
-
-            command = ['aws', 's3', '--profile', 'iarpa', 'cp']
-            if params['dry_run'] == 1:
-                command.append('--dryrun')
-
-            command.extend([local_path, upload_path])
-
-            # TODO: Manually check return code / output
-            self.logger.info("Running: {}".format(' '.join(command)))
-            if params['dry_run'] != 1:
-                subprocess.run(command, check=True)
-
-            # Update feature asset href to point to uploaded path on
-            # S3
-            feature['assets']['data']['href'] = upload_path
-
-        # Upload catalog as well
-        with tempfile.NamedTemporaryFile('w', suffix='.json') as out_catalog:
-            out_catalog.write(json.dumps(stac_catalog, indent=2))
-            out_catalog.flush()
-
-            catalog_upload_path = os.path.join(
-                params['s3_bucket'], 'catalog.json')
-
-            command = ['aws', 's3', '--profile', 'iarpa', 'cp']
-            if params['dry_run'] == 1:
-                command.append('--dryrun')
-
-            command.extend([out_catalog.name, catalog_upload_path])
-
-            # TODO: Manually check return code / output
-            self.logger.info("Running: {}".format(' '.join(command)))
-            subprocess.run(command, check=True)
-
-        cl.add_to_metadata('stac_catalog', stac_catalog)
+        cl.add_to_metadata('stac_catalog', output_stac_catalog)
         cl.add_to_metadata('s3_bucket', params['s3_bucket'])
 
         # Do not edit below this line
---
+++ watch/tools/stac_egress.py
@@ -0,0 +1,122 @@
+import argparse
+import os
+import subprocess
+import sys
+
+import pystac
+import ubelt as ub
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Align all STAC item data assets to the same CRS")
+
+    parser.add_argument('stac_catalog',
+                        type=str,
+                        help="Path to input STAC catalog")
+    parser.add_argument("--s3_bucket",
+                        type=str,
+                        required=True,
+                        help="S3 path prefix where the STAC catalog and data "
+                             "should be uploaded")
+    parser.add_argument("--dry_run",
+                        action='store_true',
+                        help="Don't actually upload, just echo AWS commands")
+
+    stac_egress(**vars(parser.parse_args()))
+
+    return 0
+
+
+def stac_egress(stac_catalog,
+                s3_bucket,
+                dry_run=False):
+    if isinstance(stac_catalog, str):
+        catalog = pystac.read_file(href=stac_catalog).full_copy()
+    else:
+        catalog = stac_catalog.full_copy()
+
+    # Ensures that root and parent paths are pointing to the uploaded
+    # path
+    catalog.normalize_hrefs(s3_bucket)
+
+    # Working directory
+    working_dir = ub.ensure_app_cache_dir('watch/tools/stac_egress_working')
+    ub.delete(working_dir)  # remove the dir and contents if it exists
+    ub.ensuredir(working_dir)  # create the empty directory.
+
+    def _item_map(stac_item):
+        item_outdir = os.path.join(s3_bucket, stac_item.id)
+        item_outpath = os.path.join(
+            item_outdir, "{}.json".format(stac_item.id))
+
+        for asset_name, asset in stac_item.assets.items():
+            asset_outpath = os.path.join(
+                item_outdir, os.path.basename(asset.href))
+
+            command = ['aws', 's3', '--profile', 'iarpa', 'cp']
+            if dry_run:
+                command.append('--dryrun')
+
+            command.extend([asset.href, asset_outpath])
+
+            # TODO: Manually check return code / output
+            subprocess.run(command, check=True)
+
+            asset.href = asset_outpath
+
+        # 'previous' links typically refer to transient items on the
+        # worker node; probably doesn't make sense to persist them by
+        # uploading to S3
+        stac_item.remove_links('previous')
+
+        stac_item.set_self_href(item_outpath)
+        stac_item_tmp_path = os.path.join(
+            working_dir,
+            stac_item.id,
+            "{}.json".format(stac_item.id))
+
+        # Write temporarily to working directory for upload
+        pystac.write_file(
+            stac_item,
+            include_self_link=True,
+            dest_href=stac_item_tmp_path)
+
+        command = ['aws', 's3', '--profile', 'iarpa', 'cp']
+        if dry_run:
+            command.append('--dryrun')
+
+        command.extend([stac_item_tmp_path, item_outpath])
+
+        # TODO: Manually check return code / output
+        subprocess.run(command, check=True)
+
+        return stac_item
+
+    output_catalog = catalog.map_items(_item_map)
+
+    output_catalog_path = os.path.join(s3_bucket, 'catalog.json')
+    output_catalog_tmp_path = os.path.join(working_dir, 'catalog.json')
+
+    # Write temporarily to working directory for upload
+    pystac.write_file(
+        output_catalog,
+        include_self_link=True,
+        dest_href=output_catalog_tmp_path)
+
+    command = ['aws', 's3', '--profile', 'iarpa', 'cp']
+    if dry_run:
+        command.append('--dryrun')
+
+    command.extend([output_catalog_tmp_path, output_catalog_path])
+
+    # TODO: Manually check return code / output
+    subprocess.run(command, check=True)
+
+    ub.delete(working_dir)  # remove the dir and contents if it exists
+
+    return output_catalog
+
+
+if __name__ == "__main__":
+    sys.exit(main())
