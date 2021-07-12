d603d832ba74b64e695a646fb9a77a615a2c537a
Jacob DeRosa <jacob.derosa@kitware.com>
2021-07-12 11:46:07 -0400

    stac to kwcoco atk algo

---
+++ atk/image_conditioning/algorithms/kitware/stac_to_kwcoco/main.py
@@ -0,0 +1,17 @@
+from algorithm_toolkit import Algorithm, AlgorithmChain
+
+from watch.tools.stac_to_kwcoco import convert
+
+class Main(Algorithm):
+
+    def run(self):
+        cl = self.cl  # type: AlgorithmChain.ChainLedger
+        params = self.params  # type: dict
+        # Add your algorithm code here
+        dset = convert(params['kwcoco'], params['catalog'])
+
+        cl.add_to_metadata('output_path', params['kwcoco'])
+        cl.add_to_metadata('dataset', dset)
+
+        # Do not edit below this line
+        return cl
