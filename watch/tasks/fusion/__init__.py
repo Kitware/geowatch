42135ec6a037ae8a0082f4a5790a293df54556dd
joncrall <erotemic@gmail.com>
2021-07-19 19:36:00 -0400

    remove artifact from local file

--- watch/tasks/fusion/__init__.py
+++ watch/tasks/fusion/__init__.py
@@ -8,11 +8,10 @@ from watch.tasks.fusion import evaluate
 from watch.tasks.fusion import fit
 from watch.tasks.fusion import fit_bigvoter
 from watch.tasks.fusion import fit_voter
-from watch.tasks.fusion import fusion_transformer_v1
 from watch.tasks.fusion import methods
 from watch.tasks.fusion import models
 from watch.tasks.fusion import predict
 from watch.tasks.fusion import utils
 
 __all__ = ['datasets', 'evaluate', 'fit', 'fit_bigvoter', 'fit_voter',
-           'fusion_transformer_v1', 'methods', 'models', 'predict', 'utils']
+           'methods', 'models', 'predict', 'utils']
