9ff4c3f013d25b6b7f22c85fee2379252b0187f8
joncrall <erotemic@gmail.com>
2021-07-28 15:08:33 -0400

    Add docs

--- watch/cli/kwcoco_to_geojson.py
+++ watch/cli/kwcoco_to_geojson.py
@@ -1,13 +1,104 @@
 """
-For documentation about the IARPA json format see [1]_. A formal json-schema
-can be found in ``watch/rc/site-model.schema.json``.
+This file contains logic to convert a kwcoco file into an IARPA Site Model.
 
-For documentation about the KWCOCO json format see [2]_. A formal json-schema
-can be found in ``kwcoco.coco_schema``
+At a glance the IARPA Site Model is a GeoJSON FeatureCollection with the
+following informal schema:
+
+list of json dictionaries, where each
+"site" has the following has the following informal schema:
+
+TODO:
+    - [ ] Is our computation of the "site-boundary" correct?
+    - [ ] Do we have a complete list of IARPA category names?
+    - [ ] Do we have a complete list of IARPA sensor names?
+    - [ ] Is our computation of the "predicted_phase" correct?
+    - [ ] How do we compute "is_occluded"?
+    - [ ] Document details about is_site_boundary
+    - [ ] Document details about is_occluded
+
+.. code::
+
+    {
+        "id": "site-000001",
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": <GeoJson Geometry>  # a polygon or multipolygon
+                "properties": {
+                    "observation_date": <iso-datetime>  # e.g. "1981-01-01",
+                    "score": <float>  # e.g. 1.0,
+                    "predicted_phase": <phase-label>  # e.g. "Active Construction",
+                    "predicted_phase_start_date": <iso-datetime>  # e.g. "1981-01-01",
+                    "is_occluded":  <comma-separated-str-of-True-False-for-each-poly>,  # e.g. "False,True"
+                    "is_site_boundary": <comma-separated-str-of-True-False-for-each-poly>,  # e.g. "False,True"
+                    "current_phase": <PhaseLabel>     # e.g. "No Activity",
+                    "sensor_name": <name-of-sensor>   # e.g. "WorldView",
+                    "source": <name-of-source-image>  # e.g. "WorldviewFile-1980-01-01.NTF"
+                },
+            },
+            ...
+        ]
+    }
+
+
+For official documentation about the KWCOCO json format see [1]_. A formal
+json-schema can be found in ``kwcoco.coco_schema``
+
+For official documentation about the IARPA json format see [2, 3]_. A formal
+json-schema can be found in ``watch/rc/site-model.schema.json``.
 
 References:
-    .. [1] https://infrastructure.smartgitlab.com/docs/pages/api_documentation.html#site-model
-    .. [2] https://gitlab.kitware.com/computer-vision/kwcoco
+    .. [1] https://gitlab.kitware.com/computer-vision/kwcoco
+    .. [2] https://infrastructure.smartgitlab.com/docs/pages/api_documentation.html#site-model
+    .. [3] https://smartgitlab.com/TE/annotations
+
+.. code:: markdown
+
+    ## Annotation Terminology
+
+    - Time slice / Image Layer:
+      - A single image capturing the scene of interest on a specific day
+    - Datacube:
+      - A collection of images from one or more sensor platforms covering the same spatial area of interest. The set of images will span some time period T = [Tstart, Tend] and include multiple images (time slices).
+    - Site / Site Boundary:
+      - A geographical area defining the spatial boundaries of large-scale change (anthropogenic or not).
+      - This is the fundamental unit of activity that SMART is focused on; it is what human annotators will be labeling and what algorithms are expected to detect and classify.
+      - For SMART Phase 1, sites of interest must be larger than 8000 m². (Note that this size is in reference to the entire site area, not the objects within the site.).
+      - There can be any number of sites within a ROI (including none)
+    - Sub-site / Sub-site Boundary:
+      - Used to indicate that an area within the site boundary is in a different activity phase as the surrounding or neighboring plots of land
+      - Sub-site boundaries are only required _**if and only if**_ the site is exhibiting multiple activity phases in a single time slice
+
+
+    ## Defining Boundaries
+
+    ### Site Boundaries
+
+    Site boundaries delineate the border of a site. They should be easily distinguishable using visible features in the imagery and should include supporting infrastructure (i.e. parking lots, pavement, etc.). Examples of features that define site boundaries include the following:
+    - Major roads and above (using [OSM definitions](https://wiki.openstreetmap.org/wiki/United_States/Road_classification))
+    - Uninhabited areas
+    - Water (rivers, ocean, etc.)
+    - Large areas of vegetation (e.g. forests)
+    - Inhabited/completed areas that don’t undergo construction in any of the views
+
+
+    ### Sub-site (intra-site) Boundaries
+
+    These are features that may be used to further split a site into sub-sites when multiple activity phases are present within the same time slice and within the bounds of a single site polygon. Sub-site/Intra-site boundaries are necessary _**if and only if**_ they separate areas that are in different construction phases at a given time slice.
+    - Completed roads (lower than ‘OSM: Major Roads’)
+    - Completed “natural” areas (e.g., open green space)
+    - Clear, visible delineations between two plots of land (only necessary if they are distinguishing between areas that are in different phases)
+
+
+    ### Site/sub-site rules
+
+    - Site boundaries should never overlap other site boundaries
+    - Sub-site boundaries will never overlap other sub-site boundaries
+    - Sub-site boundaries must not extend outside site boundaries (but they can share borders)
+    - Sub-sites must exist inside site boundaries. They cannot exist on their own.
+
+
 """
 import geojson
 import json
@@ -120,6 +211,7 @@ def convert_kwcoco_to_iarpa(coco_dset, region_id):
     Example:
         >>> from watch.cli.kwcoco_to_geojson import *  # NOQA
         >>> from watch.demo import smart_kwcoco_demodata
+        >>> import ubelt as ub
         >>> coco_dset = smart_kwcoco_demodata.demo_smart_aligned_kwcoco()
         >>> region_id = 'dummy_region'
         >>> sites = convert_kwcoco_to_iarpa(coco_dset, region_id)
@@ -135,6 +227,7 @@ def convert_kwcoco_to_iarpa(coco_dset, region_id):
     for ann in coco_dset.index.anns.values():
         img = coco_dset.index.imgs[ann['image_id']]
         cat = coco_dset.index.cats[ann['category_id']]
+        catname = cat['name']
 
         # Non-standard COCO fields, needed by watch
         sseg_geos = ann['segmentation_geos']
@@ -151,6 +244,7 @@ def convert_kwcoco_to_iarpa(coco_dset, region_id):
 
         feature = geojson.Feature(geometry=sseg_geos)
         properties = feature['properties']
+
         properties['source'] = source
         properties['observation_date'] = date.isoformat()
         # Is this default supposed to be a string?
@@ -158,7 +252,7 @@ def convert_kwcoco_to_iarpa(coco_dset, region_id):
 
         # This was supercategory, is that supposed to be name instead?
         # FIXME: what happens when the category nmames dont work?
-        properties['current_phase'] = category_dict.get(cat['name'], cat['name'])
+        properties['current_phase'] = category_dict.get(catname, catname)
 
         prediction = predict(ann, img['video_id'], coco_dset,
                              properties['current_phase'])
@@ -182,7 +276,6 @@ def convert_kwcoco_to_iarpa(coco_dset, region_id):
             # Use the first auxiliary image
             # (todo: might want to choose determine the image "canvas" coordinates?)
             img_path = join(coco_dset.bundle_dpath, img['auxiliary'][0]['file_name'])
-
         properties['is_site_boundary'] = boundary(sseg_geos, img_path)
 
         # This seems fragile? Needs docs.
