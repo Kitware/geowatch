bc402463b75dbc8b400840c875b88cf9cbb7fce6
jon.crall <jon.crall@kitware.com>
2021-08-11 16:14:12 -0400

    Finish basic eval script

--- watch/tasks/fusion/evaluate.py
+++ watch/tasks/fusion/evaluate.py
@@ -132,162 +132,211 @@ def binary_confusion_measures(tn, fp, fn, tp):
 
 
 def associate_images(true_coco, pred_coco):
-    # TODO: robust image association (see kwcoco eval)
-    common_fnames = set(true_coco.index.file_name_to_img) & set(pred_coco.index.file_name_to_img)
-    common_names = set(true_coco.index.name_to_img) & set(pred_coco.index.name_to_img)
+    # TODO: robust image/video association (see kwcoco eval)
+    common_vidnames = set(true_coco.index.name_to_video) & set(pred_coco.index.name_to_video)
 
-    import ubelt as ub
-    associated = ub.oset()
+    def image_keys(dset, gids):
+        # Generate image "keys" that should be compatible between datasets
+        for gid in gids:
+            img = dset.imgs[gid]
+            if img.get('file_name', None) is None:
+                yield img['name']
+            else:
+                yield img['file_name']
 
-    for fname in common_fnames:
-        gid1 = true_coco.index.file_name_to_img[fname]['id']
-        gid2 = pred_coco.index.file_name_to_img[fname]['id']
-        associated.add((gid1, gid2))
+    all_gids1 = list(true_coco.imgs.keys())
+    all_gids2 = list(pred_coco.imgs.keys())
+    all_keys1 = list(image_keys(true_coco, all_gids1))
+    all_keys2 = list(image_keys(pred_coco, all_gids2))
+    key_to_gid1 = ub.dzip(all_keys1, all_gids1)
+    key_to_gid2 = ub.dzip(all_keys2, all_gids2)
+    gid_to_key1 = ub.invert_dict(key_to_gid1)
+    gid_to_key2 = ub.invert_dict(key_to_gid2)
 
-    for name in common_names:
-        gid1 = true_coco.index.name_to_img[name]['id']
-        gid2 = pred_coco.index.name_to_img[name]['id']
-        associated.add((gid1, gid2))
+    video_matches = []
 
-    return associated
+    all_match_gids1 = set()
+    all_match_gids2 = set()
 
+    for vidname in common_vidnames:
+        video1 = true_coco.index.name_to_video[vidname]
+        video2 = pred_coco.index.name_to_video[vidname]
+        vidid1 = video1['id']
+        vidid2 = video2['id']
+        gids1 = true_coco.index.vidid_to_gids[vidid1]
+        gids2 = pred_coco.index.vidid_to_gids[vidid2]
+        keys1 = ub.oset(ub.dict_take(gid_to_key1, gids1))
+        keys2 = ub.oset(ub.dict_take(gid_to_key2, gids2))
+        match_keys = ub.oset(keys1) & ub.oset(keys2)
+        match_gids1 = list(ub.take(key_to_gid1, match_keys))
+        match_gids2 = list(ub.take(key_to_gid2, match_keys))
+        all_match_gids1.update(match_gids1)
+        all_match_gids2.update(match_gids2)
+        video_matches.append({
+            'vidname': vidname,
+            'match_gids1': match_gids1,
+            'match_gids2': match_gids2,
+        })
 
-def compute_simple_segmentation_metrics(true_coco, pred_coco):
+    unmatched_gid_to_key1 = ub.dict_diff(gid_to_key1, all_match_gids1)
+    unmatched_gid_to_key2 = ub.dict_diff(gid_to_key2, all_match_gids2)
 
-    assoiated_gids = associate_images(true_coco, pred_coco)
+    remain_keys = set(unmatched_gid_to_key1.values()) & set(unmatched_gid_to_key2.values())
+    remain_gids1 = [key_to_gid1[key] for key in remain_keys]
+    remain_gids2 = [key_to_gid2[key] for key in remain_keys]
 
-    confusion_matrices = ddict(dict)
-
-    for gid1, gid2 in assoiated_gids:
-        img1 = true_coco.imgs[gid1]
-        shape = (img1['height'], img1['width'])
-
-        # Create a truth "panoptic segmentation" style mask
-        true_canvas = np.zeros(shape, dtype=np.uint8)
-        true_dets = true_coco.annots(gid=gid1).detections
-        for true_sseg in true_dets.data['segmentations']:
-            true_canvas = true_sseg.fill(true_canvas, value=1)
-
-        # Create a pred "panoptic segmentation" style mask
-        pred_canvas = np.zeros(shape, dtype=np.uint8)
-        pred_dets = pred_coco.annots(gid=gid2).detections
-        for pred_sseg in pred_dets.data['segmentations']:
-            pred_canvas = pred_sseg.fill(pred_canvas, value=1)
-
-        # TODO: need to consider the fact that these annotations are
-        # multi-class, and have scores, we may want to evaluate the raw
-        # probability map in order to find a decent operating point.
-        # We also need to hook into the real IAPRA metrics.
-
-        # FIXME: for now this is just binary change
-        y_true = true_canvas.ravel()
-        y_pred = pred_canvas.ravel()
-        mat = skm.confusion_matrix(y_true, y_pred, labels=np.array([0, 1]))
-        confusion_matrices[(gid1, gid2)] = mat
-
-    rows = []
-    for (gid1, gid2), mat in confusion_matrices.items():
-        vidid1 = true_coco.imgs[gid1]['video_id']
-        video1 = true_coco.index.videos[vidid1]
-
-        tn, fp, fn, tp = mat.ravel()
-        row = binary_confusion_measures(tn, fp, fn, tp)
-        row = ub.map_vals(lambda x: x.item(), row)
-        row["gid1"] = gid1
-        row["gid2"] = gid2
-        row["video"] = video1['name']
-        rows.append(row)
-    df = pd.DataFrame(rows)
-    return df
+    image_matches = {
+        'match_gids1': remain_gids1,
+        'match_gids2': remain_gids2,
+    }
+    return video_matches, image_matches
 
 
-def plot_frames(frames, target, row, cmap, thresh=0.):
-    for ax, (channel, file_name) in zip(row, frames.items()):
-        logits = tifffile.imread(file_name)
-        pred = (logits > thresh).astype("int")
-        confusion_image = utils.confusion_image(pred, target)
-        ax.matshow(confusion_image, cmap=cmap)
+def single_image_segmentation_metrics(true_coco, pred_coco, gid1, gid2):
+    img1 = true_coco.imgs[gid1]
+    vidid1 = true_coco.imgs[gid1]['video_id']
+    video1 = true_coco.index.videos[vidid1]
+    shape = (img1['height'], img1['width'])
 
-        #  ax.set_axis_off()
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.patch.set_edgecolor('black')
-        ax.patch.set_linewidth('1')
+    # Create a truth "panoptic segmentation" style mask
+    true_canvas = np.zeros(shape, dtype=np.uint8)
+    true_dets = true_coco.annots(gid=gid1).detections
+    for true_sseg in true_dets.data['segmentations']:
+        true_canvas = true_sseg.fill(true_canvas, value=1)
 
-        ax.set_title(channel)
+    # Create a pred "panoptic segmentation" style mask
+    pred_canvas = np.zeros(shape, dtype=np.uint8)
+    pred_dets = pred_coco.annots(gid=gid2).detections
+    for pred_sseg in pred_dets.data['segmentations']:
+        pred_canvas = pred_sseg.fill(pred_canvas, value=1)
+
+    # TODO: need to consider the fact that these annotations are
+    # multi-class, and have scores, we may want to evaluate the raw
+    # probability map in order to find a decent operating point.
+    # We also need to hook into the real IAPRA metrics.
+
+    # FIXME: for now this is just binary change
+    y_true = true_canvas.ravel()
+    y_pred = pred_canvas.ravel()
+    mat = skm.confusion_matrix(y_true, y_pred, labels=np.array([0, 1]))
+
+    info = {
+        'mat': mat,
+        'pred_canvas': pred_canvas,
+        'true_canvas': true_canvas,
+    }
+    tn, fp, fn, tp = mat.ravel()
+    row = binary_confusion_measures(tn, fp, fn, tp)
+    row = ub.map_vals(lambda x: x.item(), row)
+    row["true_gid"] = gid1
+    row["pred_gid"] = gid2
+    row["video"] = video1['name']
+    info['row'] = row
+    return info
 
 
-def make_confusion_plots(true_coco, pred_coco, figure_root):
+@ub.memoize
+def _memo_legend(label_to_color):
     import kwplot
-    with kwplot.BackendContext('Agg'):
-        import matplotlib.pyplot as plt
-        from matplotlib import colors
-        import matplotlib.patches as mpatches
-
-        cmap = colors.ListedColormap(["w", "k", "r", "y"], N=4)
-
-        results = kwcoco.CocoDataset(str(result_kwcoco_path))
-
-        models = sorted(list({
-            aux["channels"].rsplit("_", 1)[0]
-            for aux in results.dataset["images"][0]["auxiliary"]
-        }))
-
-        for video in tqdm.tqdm(results.dataset["videos"]):
-            for model_name in tqdm.tqdm(models):
-
-                # if video["name"] not in test_cities: continue
-
-                images = [
-                    image
-                    for image in results.dataset["images"]
-                    if image["video_id"] == video["id"]
-                ]
-
-                H = len(images)
-                W = len(images[0]["auxiliary"])
-
-                fig, axs = plt.subplots(H, W, figsize=(3 * W, 4 * H), squeeze=False)
-                if len(axs.shape) == 1:
-                    axs = axs[None]
-
-                for idx, (row, image) in enumerate(zip(axs, images)):
-
-                    frames = {
-                        aux["channels"].rsplit("_", 1)[-1]: aux["file_name"]
-                        for aux in image["auxiliary"]
-                    }
-
-                    target = np.zeros([video["height"], video["width"]])
-
-                    for ann_id in results.gid_to_aids[image["id"]]:
-                        seg = kwimage.Segmentation.coerce(
-                            results.anns[ann_id]["segmentation"]
-                        ).to_multi_polygon()
-                        seg.fill(target, value=1)
-
-                    target = target.astype("int")
-
-                    plot_frames(frames, target, row, cmap, thresh=0.)
-
-                patches = [
-                    mpatches.Patch(color=cmap.colors[idx], label=name)
-                    for idx, name in enumerate(["TN", "TP", "FN", "FP"])
-                ]
-                fig.legend(handles=patches, loc='upper center', ncol=4, fontsize="xx-large", bbox_to_anchor=(0.5, 0.))
-
-                # params = list(frames.values())[0]["params"]
-                # fig.suptitle(f"{video['name']} / {model_name} ({millify(params)})", fontsize="xx-large")
-                fig.suptitle(f"{video['name']} / {model_name}", fontsize="xx-large")
-                fig.tight_layout()
-                fig.patch.set_facecolor('white')
-
-                figure_path = figure_root / video['name']
-                figure_path.mkdir(parents=True, exist_ok=True)
-                fig.savefig(figure_path / (model_name + ".png"))
+    legend_img = kwplot.make_legend_img(label_to_color)
+    return legend_img
 
 
-def evaluate_segmentations(true_dataset, pred_dataset, eval_dpath=None):
+def dump_chunked_confusion(true_coco, pred_coco, chunk_info, plot_dpath):
+    """
+    Draw a a sequence of true/pred image predictions
+    """
+    colors = ["white", "green", "yellow", "red"]
+    color_labels = ["TN", "TP", "FN", "FP"]
+    color_lut = np.array([kwimage.Color(c).as255() for c in colors])
+
+    # Make a legend
+    color01_lut = color_lut / 255.0
+    label_to_color = ub.dzip(color_labels, color01_lut)
+    legend_img = _memo_legend(label_to_color)
+    legend_img = kwimage.ensure_uint255(legend_img)
+
+    # Draw predictions on each frame
+    parts = []
+    frame_nums = []
+    true_gids = []
+    unique_vidnames = set()
+    for info in chunk_info:
+        row = info['row']
+        unique_vidnames.add(row['video'])
+        true_gid = row['true_gid']
+        img = true_coco.index.imgs[true_gid]
+        frame_index = img['frame_index']
+        frame_nums.append(frame_index)
+        true_gids.append(true_gid)
+
+        # pred_gid = row['pred_gid']
+
+        pred_canvas = info['pred_canvas']
+        true_canvas = info['true_canvas']
+        confusion_idxs = utils.confusion_image(pred_canvas, true_canvas)
+        confusion_image = color_lut[confusion_idxs]
+
+        image_text = f'{frame_index} - gid = {true_gid}'
+
+        confusion_image = kwimage.ensure_uint255(confusion_image)
+        confusion_image = kwimage.draw_text_on_image(
+            confusion_image, image_text, org=(1, 1), valign='top',
+            color='white', border={'color': 'black'})
+
+        parts.append(confusion_image)
+
+    max_frame = min(frame_nums)
+    min_frame = max(frame_nums)
+    max_gid = max(true_gids)
+    min_gid = min(true_gids)
+
+    if max_frame == min_frame:
+        frame_part = f'{min_frame}'
+    else:
+        frame_part = f'{min_frame}-{max_frame}'
+
+    if max_gid == min_gid:
+        gid_part = f'{min_gid}'
+    else:
+        gid_part = f'{min_gid}-{max_gid}'
+    vidname_part = '_'.join(list(unique_vidnames))
+
+    plot_fname = f'{vidname_part}-{frame_part}-{gid_part}.png'
+    plot_canvas = kwimage.stack_images(parts, axis=1, overlap=-10)
+
+    plot_canvas = kwimage.stack_images([plot_canvas, legend_img], axis=1, overlap=-10)
+
+    header = header_text(plot_fname, max_dim=plot_canvas.shape[1], shrink=False)
+    plot_canvas = kwimage.stack_images([header, plot_canvas], axis=0)
+
+    plot_dpath = pathlib.Path(str(plot_dpath))
+    plot_fpath = plot_dpath / plot_fname
+    kwimage.imwrite(str(plot_fpath), plot_canvas)
+
+
+def header_text(text, max_dim=None, shrink=False):
+    """
+    If shrink is true, shrinks the text to fit, otherwise text is
+    placed in the center at a constant size, but is not guarenteed
+    to fit.
+    """
+    import cv2
+    if shrink:
+        header = kwimage.draw_text_on_image(
+            None, text, org=(1, 1),
+            valign='top', halign='left', color='salmon')
+        header = cv2.copyMakeBorder(header, 3, 3, 3, 3,
+                                    cv2.BORDER_CONSTANT)
+        header = kwimage.imresize(header, dsize=(max_dim, None))
+    else:
+        header = kwimage.draw_text_on_image(
+            {'width': max_dim}, text, org=(max_dim // 2, 1),
+            valign='top', halign='center', color='salmon')
+    return header
+
+
+def evaluate_segmentations(true_coco, pred_coco, eval_dpath=None):
     """
 
     Example:
@@ -295,21 +344,60 @@ def evaluate_segmentations(true_dataset, pred_dataset, eval_dpath=None):
         >>> from kwcoco.coco_evaluator import CocoEvaluator
         >>> from kwcoco.demo.perterb import perterb_coco
         >>> import kwcoco
-        >>> true_dataset = kwcoco.CocoDataset.demo('vidshapes8')
+        >>> true_coco = kwcoco.CocoDataset.demo('vidshapes8')
         >>> kwargs = {
         >>>     'box_noise': 0.5,
         >>>     'n_fp': (0, 10),
         >>>     'n_fn': (0, 10),
         >>>     'with_probs': True,
         >>> }
-        >>> pred_dataset = perterb_coco(true_dataset, **kwargs)
-        >>> evaluate_segmentations(true_dataset, pred_dataset)
+        >>> pred_coco = perterb_coco(true_coco, **kwargs)
+        >>> eval_dpath = ub.ensure_app_cache_dir('watch/tests/fusion_eval')
+        >>> print('eval_dpath = {!r}'.format(eval_dpath))
+        >>> evaluate_segmentations(true_coco, pred_dataset, eval_dpath)
     """
-    import kwcoco
-    true_coco = kwcoco.CocoDataset.coerce(true_dataset)
-    pred_coco = kwcoco.CocoDataset.coerce(pred_dataset)
+    video_matches, image_matches = associate_images(true_coco, pred_coco)
+    rows = []
+    chunk_size = 5
 
-    df = compute_simple_segmentation_metrics(true_coco, pred_coco)
+    if eval_dpath is None:
+        plot_dpath = None
+        draw = False
+    else:
+        plot_dpath = pathlib.Path(eval_dpath) / 'plots'
+        plot_dpath.mkdir(exist_ok=True, parents=True)
+        draw = True
+
+    # Handle images in videos
+    for video_match in video_matches:
+        gids1 = video_match['match_gids1']
+        gids2 = video_match['match_gids2']
+        pairs = list(zip(gids1, gids2))
+        for chunk in ub.chunks(pairs, chunk_size):
+            chunk_info = []
+            for gid1, gid2 in chunk:
+                info = single_image_segmentation_metrics(
+                    true_coco, pred_coco, gid1, gid2)
+                rows.append(info['row'])
+                if draw:
+                    chunk_info.append(info)
+            if draw:
+                dump_chunked_confusion(
+                    true_coco, pred_coco, chunk_info, plot_dpath)
+
+    # Handle standalone images
+    gids1 = image_matches['match_gids1']
+    gids2 = image_matches['match_gids2']
+    for gid1, gid2 in zip(gids1, gids2):
+        info = single_image_segmentation_metrics(
+            true_coco, pred_coco, gid1, gid2)
+        rows.append(info['row'])
+        if draw:
+            chunk_info = [info]
+            dump_chunked_confusion(
+                true_coco, pred_coco, chunk_info, plot_dpath)
+
+    df = pd.DataFrame(rows)
     print(df)
 
     if eval_dpath is not None:
@@ -317,8 +405,7 @@ def evaluate_segmentations(true_dataset, pred_dataset, eval_dpath=None):
         eval_dpath.mkdir(exist_ok=True, parents=True)
         metrics_fpath = eval_dpath / 'metrics.json'
         df.to_csv(str(metrics_fpath), index=False)
-    # make_confusion_plots(args.eval_dpath, args.figure_root)
-    # compute_metrics(args.result_kwcoco_path, args.metrics_path)
+    return df
 
 
 def main():
@@ -329,7 +416,10 @@ def main():
     parser.add_argument("--eval_dpath", help='path to dump results')
     args = parser.parse_args()
 
-    evaluate_segmentations(args.true_dataset, args.pred_dataset, args.eval_dpath)
+    true_coco = kwcoco.CocoDataset.coerce(args.true_dataset)
+    pred_coco = kwcoco.CocoDataset.coerce(args.pred_dataset)
+    eval_dpath = args.eval_dpath
+    evaluate_segmentations(true_coco, pred_coco, eval_dpath)
 
 
 if __name__ == "__main__":
