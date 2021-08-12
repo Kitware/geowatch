ec46bed7371d2fa8680a5a1f28c9d11ae35276f3
jon.crall <jon.crall@kitware.com>
2021-08-12 10:41:41 -0400

    fix linter

--- watch/tasks/fusion/datamodules/__init__.py
+++ watch/tasks/fusion/datamodules/__init__.py
@@ -12,21 +12,19 @@ __submodules__ = [
     'project_data',
 
 ]
-from watch.tasks.fusion.datamodules.common import (AddPositionalEncoding,
-                                                   VideoDataset,)
+from watch.tasks.fusion.datamodules.common import (VideoDataset,)
 from watch.tasks.fusion.datamodules.sun_rgbd import (SUN_RGBD,
                                                      SUN_RGBD_Dataset,)
-from watch.tasks.fusion.datamodules.watch_data import (AddPositionalEncoding,
-                                                       WatchDataModule,
+from watch.tasks.fusion.datamodules.watch_data import (WatchDataModule,
                                                        WatchVideoDataset,
                                                        category_tree_ensure_color,
                                                        coco_channel_profiles,
                                                        morphology, profile,
                                                        simple_video_sample_grid,)
 from watch.tasks.fusion.datamodules.project_data import (Drop0AlignMSI_S2,
-    Drop0Raw_S2,)
+                                                         Drop0Raw_S2,)
 
-__all__ = ['AddPositionalEncoding', 'Drop0AlignMSI_S2', 'Drop0Raw_S2',
+__all__ = ['Drop0AlignMSI_S2', 'Drop0Raw_S2',
            'SUN_RGBD', 'SUN_RGBD_Dataset', 'VideoDataset', 'WatchDataModule',
            'WatchVideoDataset', 'category_tree_ensure_color',
            'coco_channel_profiles', 'morphology', 'profile',
