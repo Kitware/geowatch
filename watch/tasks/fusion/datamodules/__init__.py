56da64c2bccc36ffb29d7ffa8d2f82adab72517c
jon.crall <jon.crall@kitware.com>
2021-08-12 10:25:37 -0400

    Fix tests

--- watch/tasks/fusion/datamodules/__init__.py
+++ watch/tasks/fusion/datamodules/__init__.py
@@ -4,13 +4,16 @@ python -m watch.tasks.fusion
 
 mkinit ~/code/watch/watch/tasks/fusion/datamodules/__init__.py --nomods -w
 """
+
+__submodules__ = [
+    'common',
+    'sun_rgbd',
+    'watch_data',
+    'project_data',
+
+]
 from watch.tasks.fusion.datamodules.common import (AddPositionalEncoding,
                                                    VideoDataset,)
-from watch.tasks.fusion.datamodules.drop0_align_msi_s2_preprocess_split import (
-    main,)
-from watch.tasks.fusion.datamodules.onera_2018 import (OneraCD_2018,)
-from watch.tasks.fusion.datamodules.project_data import (Drop0AlignMSI_S2,
-    Drop0Raw_S2,)
 from watch.tasks.fusion.datamodules.sun_rgbd import (SUN_RGBD,
                                                      SUN_RGBD_Dataset,)
 from watch.tasks.fusion.datamodules.watch_data import (AddPositionalEncoding,
@@ -20,9 +23,11 @@ from watch.tasks.fusion.datamodules.watch_data import (AddPositionalEncoding,
                                                        coco_channel_profiles,
                                                        morphology, profile,
                                                        simple_video_sample_grid,)
+from watch.tasks.fusion.datamodules.project_data import (Drop0AlignMSI_S2,
+    Drop0Raw_S2,)
 
 __all__ = ['AddPositionalEncoding', 'Drop0AlignMSI_S2', 'Drop0Raw_S2',
-           'OneraCD_2018', 'SUN_RGBD', 'SUN_RGBD_Dataset', 'VideoDataset',
-           'WatchDataModule', 'WatchVideoDataset',
-           'category_tree_ensure_color', 'coco_channel_profiles', 'main',
-           'morphology', 'profile', 'simple_video_sample_grid']
+           'SUN_RGBD', 'SUN_RGBD_Dataset', 'VideoDataset', 'WatchDataModule',
+           'WatchVideoDataset', 'category_tree_ensure_color',
+           'coco_channel_profiles', 'morphology', 'profile',
+           'simple_video_sample_grid']
