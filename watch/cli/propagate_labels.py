8f1a8558e0a3b29460e552a5084419ee5edf818f
Usman Rafique <usman.rafique@horologic.khq.kitware.com>
2021-08-17 15:06:55 -0400

    fix image loading and optimize code

--- watch/cli/propagate_labels.py
+++ watch/cli/propagate_labels.py
@@ -29,20 +29,9 @@ import ubelt as ub
 import numpy as np
 
 
-def get_canvas_concat_channels(red_fname, annotations, dataset):
-    # returns a canvas with RGB image and overlaid annotation
-    
-    # Takes in name of the red channel file, reads R, G, and B files, normalizes every channel, and concatenates
-    # all 3 channels to make an RGB image
-    r_img = kwimage.normalize_intensity(kwimage.imread(red_fname))
-
-    g_name = red_fname[:-7]+'green.tif'
-    g_img = kwimage.normalize_intensity(kwimage.imread(g_name))
-
-    b_name =red_fname[:-7]+'blue.tif'
-    b_img = kwimage.normalize_intensity(kwimage.imread(b_name))
-    
-    canvas = np.concatenate((r_img[:,:,np.newaxis], g_img[:,:,np.newaxis], b_img[:,:,np.newaxis]), axis=-1)
+def get_canvas_concat_channels(annotations, dataset, img_id):
+    canvas = dataset.delayed_load(img_id, channels='red|green|blue').finalize()
+    canvas = kwimage.normalize_intensity(canvas)
     canvas = kwimage.ensure_float01(canvas)
 
     dets = kwimage.Detections.from_coco_annots(annotations, dset=dataset)
@@ -73,40 +62,28 @@ def save_visualizations(canvases, canvases_fixed, fname):
     plt.close()
     
 
-def main(**args):
+def main(args):
     """
     Main function for propagate_labels.
 
     Example:
         >>> from watch.cli.propagate_labels import *  # NOQA
-        >>> dataset_fname = ''
-        >>> python -m watch.demo.propagate_labels --in_file=dataset_fname --out_dir=processed
+        >>> dataset_directory = 'drop1-S2-aligned-c1'
+        >>> python -m watch.demo.propagate_labels --data_dir=dataset_directory --out_dir='propagation_output'
 
     """
-    # Settings
-    
-    # which categories we want to propagate
-    # These are category IDs: 1:'No Activity', 2:'Site Preparation', 3:'Active Construction', 4:'Post Construction'}, 5:'Unknown'}
-    categories_to_propagate = [2, 3]
-    
-    # number of visualizations of every sequence 
-    n_image_viz = 7
-    
-    # we save the ending frames of every video sequence if this is set to True 
-    viz_end = True
-    
-    
     # read arguments
     parser = argparse.ArgumentParser(
         description="Forward propagate labels")
     parser.add_argument("--data_dir", default= 'drop1-S2-aligned-c1', 
     help="drop1 directory name, defualt is drop1-S2-aligned-c1. The kwcoco file from this directory will be read")
     parser.add_argument("--out_dir", default='propagation_output', help="Output directory where visualizations and processed kwcoco files will be saved")
+    parser.add_argument("--viz_end", default=False, action="store_true", help="if True, last few frames will be saved")
+    parser.add_argument("--verbose", default=False, action="store_true", help="use this to print details")
     args = parser.parse_args(args)
     
     # create the output dir
-    if not os.path.exists(args.out_dir):
-        os.mkdir(args.out_dir)
+    os.makedirs(args.out_dir, exist_ok=True)
     
     # Read input file
     _default = ub.expandpath('$HOME/data/dvc-repos/smart_watch_dvc')
@@ -117,13 +94,26 @@ def main(**args):
     print('total images:', full_ds.n_images)
     print('total annotations:', full_ds.n_annots)
     
+    # which categories we want to propagate
+    catnames_to_propagate = [
+        'Site Preparation',
+        'Active Construction',]
+    categories_to_propagate = [full_ds.index.name_to_cat[c]['id'] for c in catnames_to_propagate]    
+    
+    # number of visualizations of every sequence 
+    n_image_viz = 7
+    
+    # we save the ending frames of every video sequence if this is set to True 
+    # viz_end = True
+    # deprecating from code, this is now passed as an argument
+    
     
     for vid_id, video in full_ds.index.videos.items():
         image_ids = full_ds.index.vidid_to_gids[vid_id]
 
-        # a list of all the seen track IDs
-        seen_track_ids = []   
-        # a doctionary of latest annotation IDs, indexed by the track IDs
+        # a set of all the seen track IDs
+        seen_track_ids = set()
+        # a dictionary of latest annotation IDs, indexed by the track IDs
         latest_ann_ids = {}   
 
         # canvases for visualizations
@@ -132,51 +122,49 @@ def main(**args):
 
         # for all images in this video
         for j, img_id in enumerate(image_ids):
-            this_track_ids = []
+            this_track_ids = set()
             this_image_anns = []
 
             this_image_fixed_anns = []
 
             aids = full_ds.gid_to_aids[img_id]
-            # currently reading the red channel name as that is always avaialable
-            r_fnames = [aux['file_name'] for aux in full_ds.imgs[img_id]['auxiliary'] if aux['channels'] == 'red']
 
             for aid in list(aids):
                 anns = full_ds.anns[aid]
                 this_image_anns.append(anns)
                 track_id = anns['track_id']
                 if track_id not in this_track_ids:
-                    this_track_ids.append(track_id)
+                    this_track_ids.update({track_id})
 
                 latest_ann_ids[track_id] = aid
 
             # add any track IDs to the list of seen track ids
-            new_track_ids = [i for i in this_track_ids if i not in seen_track_ids]
+            new_track_ids = this_track_ids - seen_track_ids
             if new_track_ids:
-                seen_track_ids.extend(new_track_ids)
+                seen_track_ids.update(new_track_ids)
 
             this_image_fixed_anns = this_image_anns.copy()  # if there is anything missing, we are going to fix now
 
             # was there any seen track ID that was not in this image?
-            missing_track_ids = [i for i in seen_track_ids if i not in this_track_ids]
+            missing_track_ids = seen_track_ids - this_track_ids
             if missing_track_ids:
 
                 for missing in missing_track_ids:
                     if full_ds.anns[latest_ann_ids[missing]]['category_id'] in categories_to_propagate :
                         # check if the annotation belongs to the list of categories that we want to propagate
                         this_image_fixed_anns.append( full_ds.anns[latest_ann_ids[missing]] )
-                        print('adding category:', full_ds.anns[latest_ann_ids[missing]]['category_id'])
+                        if args.verbose:
+                            print('added annotation for image', img_id, 'track ID', missing)
 
             # Get "n_image_viz" number of canvases for visualization with original annotations
-            store_starting_frame = viz_end and ((video['num_frames'] - j) <=  n_image_viz)
-            store_ending_frame = (not viz_end) and (j < n_image_viz)
+            store_starting_frame = args.viz_end and ((video['num_frames'] - j) <=  n_image_viz)
+            store_ending_frame = (not args.viz_end) and (j < n_image_viz)
             if store_starting_frame or store_ending_frame:
-                 image_full_fname = join(dvc_dpath, args.data_dir, r_fnames[0])
-                 canvases.append(get_canvas_concat_channels(image_full_fname, annotations=this_image_anns, dataset=full_ds))
-                 canvases_fixed.append(get_canvas_concat_channels(image_full_fname, annotations=this_image_fixed_anns, dataset=full_ds))
+                 canvases.append(get_canvas_concat_channels(annotations=this_image_anns, dataset=full_ds, img_id=img_id))
+                 canvases_fixed.append(get_canvas_concat_channels(annotations=this_image_fixed_anns, dataset=full_ds, img_id=img_id))
 
         # save visualization
-        location_string = '_end' if viz_end else '_start'
+        location_string = '_end' if args.viz_end else '_start'
         fname = join(args.out_dir, 'video_'+str(vid_id)+location_string+'.jpg')
         save_visualizations(canvases, canvases_fixed, fname)
     
