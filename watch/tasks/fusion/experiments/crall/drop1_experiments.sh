fe387394712a4f7c4509b1978fc0ffea3db2cfbd
joncrall <erotemic@gmail.com>
2021-08-20 23:02:01 -0400

    wip

--- watch/tasks/fusion/experiments/crall/drop1_experiments.sh
+++ watch/tasks/fusion/experiments/crall/drop1_experiments.sh
@@ -1,5 +1,28 @@
 # Takes ~18GB on a 3090
 
+DVC_DPATH=$HOME/data/dvc-repos/smart_watch_dvc 
+mkdir -p $DVC_DPATH/training/$HOSTNAME/$USER/Drop1_S2_L8_GSD10/configs
+python -m watch.tasks.fusion.fit \
+    --channels="coastal|blue|green|red|nir|swir16|swir22" \
+    --method="MultimodalTransformerDirectCD" \
+    --arch_name=smt_it_stm_p8 \
+    --time_steps=8 \
+    --chip_size=128 \
+    --batch_size=2 \
+    --accumulate_grad_batches=8 \
+    --num_workers=4 \
+    --gpus=1  \
+    --learning_rate=1e-3 \
+    --weight_decay=1e-4 \
+    --dropout=0.1 \
+    --window_size=8 \
+    --train_dataset=$DVC_DPATH/drop1-S2-L8-aligned-c1/train_gsd10_data.kwcoco.json \
+    --vali_dataset=$DVC_DPATH/drop1-S2-L8-aligned-c1/vali_gsd10_data.kwcoco.json \
+    --default_root_dir=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_S2_L8_GSD10/runs/DirectCD_smt_it_stm_s12_v3 \
+       --package_fpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_S2_L8_GSD10/runs/DirectCD_smt_it_stm_s12_v3/final_package.pt \
+                --dump=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_S2_L8_GSD10/configs/DirectCD_smt_it_stm_s12_v3.yml 
+
+
 
 
 # Build the experiment configs
@@ -142,3 +165,84 @@ python -m watch.tasks.fusion.evaluate
         --true_dataset=$DVC_DPATH/drop1-S2-L8-aligned-c1/vali_data.kwcoco.json \
         --pred_dataset=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V1/DirectCD_smt_it_stm_s12_v3/pred.kwcoco.json
           --eval_dpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V1/DirectCD_smt_it_stm_s12_v3/eval
+
+
+#####  TEAMFEATS V2  #####
+
+
+#CHANNEL_SPEC="blue|inv_sort1|inv_sort2|inv_sort3|inv_sort4|inv_sort5|inv_sort6|inv_sort7|inv_sort8|B05|B07|swir16|B09|nir|coastal|B06|inv_overlap1|inv_overlap2|inv_overlap3|inv_overlap4|inv_overlap5|inv_overlap6|inv_overlap7|inv_overlap8|inv_shared1|inv_shared2|inv_shared3|inv_shared4|inv_shared5|inv_shared6|inv_shared7|inv_shared8|inv_shared9|inv_shared10|inv_shared11|inv_shared12|inv_shared13|inv_shared14|inv_shared15|inv_shared16|inv_shared17|inv_shared18|inv_shared19|inv_shared20|inv_shared21|inv_shared22|inv_shared23|inv_shared24|inv_shared25|inv_shared26|inv_shared27|inv_shared28|inv_shared29|inv_shared30|inv_shared31|inv_shared32|inv_shared33|inv_shared34|inv_shared35|inv_shared36|inv_shared37|inv_shared38|inv_shared39|inv_shared40|inv_shared41|inv_shared42|inv_shared43|inv_shared44|inv_shared45|inv_shared46|inv_shared47|inv_shared48|inv_shared49|inv_shared50|inv_shared51|inv_shared52|inv_shared53|inv_shared54|inv_shared55|inv_shared56|inv_shared57|inv_shared58|inv_shared59|inv_shared60|inv_shared61|inv_shared62|inv_shared63|inv_shared64|inv_augment1|inv_augment2|inv_augment3|inv_augment4|inv_augment5|inv_augment6|inv_augment7|inv_augment8|red|cirrus|swir22|B8A|green"
+
+watch-cli watch_coco_stats $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10.kwcoco.json
+
+kwcoco subset --src $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10.kwcoco.json \
+        --dst $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json \
+        --select_videos '.name | startswith("KR_")'
+
+kwcoco subset --src $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10.kwcoco.json \
+        --dst $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10_train.kwcoco.json \
+        --select_videos '.name | startswith("KR_") | not'
+
+kwcoco stats \
+    $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10_train.kwcoco.json \
+    $HOME/data/dvc-repos/smart_watch_dvc/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json
+
+CHANNEL_SPEC="coastal|green"
+
+DVC_DPATH=$HOME/data/dvc-repos/smart_watch_dvc 
+python -m watch.tasks.fusion.fit \
+    --channels="$CHANNEL_SPEC" \
+    --method="MultimodalTransformerDirectCD" \
+    --arch_name=smt_it_stm_p8 \
+    --time_steps=4 \
+    --chip_size=96 \
+    --batch_size=1 \
+    --accumulate_grad_batches=8 \
+    --num_workers=4 \
+    --gpus=1  \
+    --learning_rate=1e-3 \
+    --weight_decay=1e-4 \
+    --dropout=0.1 \
+    --window_size=8 \
+    --dump=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/configs/DirectCD_smt_it_stm_s12_v4.yml 
+
+cat $DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/configs/DirectCD_smt_it_stm_s12_v4.yml 
+
+DVC_DPATH=$HOME/data/dvc-repos/smart_watch_dvc
+python -m watch.tasks.fusion.fit \
+              --config=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/configs/DirectCD_smt_it_stm_s12_v4.yml \
+    --default_root_dir=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/runs/DirectCD_smt_it_stm_s12_v4 \
+       --package_fpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/runs/DirectCD_smt_it_stm_s12_v4/final_package.pt  \
+        --train_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_train.kwcoco.json \
+        --vali_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json 
+        #--test_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json \
+
+python -m watch.tasks.fusion.predict 
+        --test_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json \
+       --package_fpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/DirectCD_smt_it_stm_s12_v4/final_package.pt \
+        --pred_dataset=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/DirectCD_smt_it_stm_s12_v4/pred.kwcoco.json
+
+python -m watch.tasks.fusion.evaluate 
+        --true_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json \
+        --pred_dataset=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/DirectCD_smt_it_stm_s12_v4/pred.kwcoco.json
+          --eval_dpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/DirectCD_smt_it_stm_s12_v4/eval
+
+
+DVC_DPATH=$HOME/data/dvc-repos/smart_watch_dvc
+python -m watch.tasks.fusion.fit \
+    --method="MultimodalTransformerDirectCD" \
+    --arch_name=smt_it_stm_p8 \
+    --time_steps=4 \
+    --channels="inv_augment1|inv_augment2|inv_augment3|inv_augment4|inv_augment5|inv_augment6|inv_augment7|inv_augment8" \
+    --chip_size=96 \
+    --batch_size=1 \
+    --accumulate_grad_batches=8 \
+    --num_workers=4 \
+    --gpus=1  \
+    --learning_rate=1e-3 \
+    --weight_decay=1e-4 \
+    --dropout=0.1 \
+    --window_size=8 \
+    --default_root_dir=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/runs/DirectCD_smt_it_stm_s12_v4 \
+       --package_fpath=$DVC_DPATH/training/$HOSTNAME/$USER/Drop1_TeamFeats_V2/runs/DirectCD_smt_it_stm_s12_v4/final_package.pt  \
+        --train_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_train.kwcoco.json \
+        --vali_dataset=$DVC_DPATH/drop1-S2-L8-aligned-old/data_gsd10_vali.kwcoco.json 
