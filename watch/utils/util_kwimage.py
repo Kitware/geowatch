80b1fd6c65155f0fc732e027e76762d9e92f3445
jon.crall <jon.crall@kitware.com>
2021-09-09 11:38:10 -0400

    Add draw_header_text utility

--- watch/utils/util_kwimage.py
+++ watch/utils/util_kwimage.py
@@ -3,7 +3,7 @@ These functions might be added to kwimage
 """
 
 
-def draw_header_text(image, text, fit=False, color='white'):
+def draw_header_text(image, text, fit=False, color='red', halign='center'):
     """
     Places a black bar on top of an image and writes text in it
 
@@ -11,16 +11,23 @@ def draw_header_text(image, text, fit=False, color='white'):
         >>> from watch.utils.util_kwimage import *  # NOQA
         >>> import kwimage
         >>> image = kwimage.grab_test_image()
-        >>> canvas1 = draw_header_text(image, 'a long header' * 5, fit=False)
-        >>> canvas2 = draw_header_text(image, 'a long header' * 5, fit=True)
+        >>> canvases = []
+        >>> canvases += [draw_header_text(image, 'unfit long header ' * 5, fit=False)]
+        >>> canvases += [draw_header_text(image, 'shrunk long header ' * 5, fit='shrink')]
+        >>> canvases += [draw_header_text(image, 'left header', fit=False, halign='left')]
+        >>> canvases += [draw_header_text(image, 'center header', fit=False, halign='center')]
+        >>> canvases += [draw_header_text(image, 'right header', fit=False, halign='right')]
+        >>> canvases += [draw_header_text(image, 'shrunk header', fit='shrink', halign='left')]
+        >>> canvases += [draw_header_text(image, 'fit header', fit=True, halign='left')]
         >>> # xdoctest: +REQUIRES(--show)
         >>> import kwplot
         >>> kwplot.autompl()
-        >>> kwplot.imshow(canvas1, pnum=(1, 2, 1))
-        >>> kwplot.imshow(canvas2, pnum=(1, 2, 2))
+        >>> pnum_ = kwplot.PlotNums(nSubplots=len(canvases))
+        >>> for c in canvases:
+        >>>     kwplot.imshow(c, pnum=pnum_())
         >>> kwplot.show_if_requested()
     """
-    import cv2
+    # import cv2
     import kwimage
     width = image.shape[1]
     if fit:
@@ -28,13 +35,28 @@ def draw_header_text(image, text, fit=False, color='white'):
         header = kwimage.draw_text_on_image(
             None, text, org=(1, 1),
             valign='top', halign='left', color=color)
-        header = cv2.copyMakeBorder(header, 3, 3, 3, 3,
-                                    cv2.BORDER_CONSTANT)
-        header = kwimage.imresize(header, dsize=(width, None))
+        # header = cv2.copyMakeBorder(header, 3, 3, 3, 3,
+        #                             cv2.BORDER_CONSTANT)
+
+        if fit == 'shrink':
+            if header.shape[1] > width:
+                header = kwimage.imresize(header, dsize=(width, None))
+        else:
+            header = kwimage.imresize(header, dsize=(width, None))
     else:
+        # Allows for howeverm much height is needed
+        if halign == 'left':
+            org = (1, 1)
+        elif halign == 'center':
+            org = (width // 2, 1)
+        elif halign == 'right':
+            org = (width - 1, 1)
+        else:
+            raise KeyError(halign)
+
         header = kwimage.draw_text_on_image(
-            {'width': width}, text, org=(width // 2, 1),
-            valign='top', halign='center', color=color)
+            {'width': width}, text, org=org,
+            valign='top', halign=halign, color=color)
 
     stacked = kwimage.stack_images([header, image], axis=0, overlap=-1)
     return stacked
