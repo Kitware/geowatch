a065d83b1af0f14e0a3a42fe400fd8310f513646
joncrall <erotemic@gmail.com>
2021-09-14 13:49:26 -0400

    wip

--- watch/tasks/fusion/experiments/crall/drop1_2021.sh
+++ watch/tasks/fusion/experiments/crall/drop1_2021.sh
@@ -231,9 +231,14 @@ python -m watch.tasks.fusion.fit \
     --torch_sharing_strategy=default \
     --torch_start_method=default \
     --num_sanity_val_steps=0 \
-    --init=$HOME/remote/yardrat/data/dvc-repos/smart_watch_dvc/training/yardrat/jon.crall/Drop1RawLeftRight/runs/DirectCD_smt_it_joint_p8_raw7common_v4/lightning_logs/version_1/package-interupt/package_epoch38_step26579.pt \
+    --init=$HOME/remote/yardrat/smart_watch_dvc/training/yardrat/jon.crall/Drop1RawLeftRight/runs/DirectCD_smt_it_joint_p8_raw7common_v4/lightning_logs/version_2/checkpoints/epoch=2-step=1241-v3.ckpt \
     --default_root_dir=$DEFAULT_ROOT_DIR \
        --package_fpath=$PACKAGE_FPATH \
         --train_dataset=$TRAIN_FPATH \
          --vali_dataset=$VALI_FPATH \
          --test_dataset=$TEST_FPATH 
+
+--init=$HOME/remote/yardrat/data/dvc-repos/smart_watch_dvc/training/yardrat/jon.crall/Drop1RawLeftRight/runs/DirectCD_smt_it_joint_p8_raw7common_v4/lightning_logs/version_1/package-interupt/package_epoch38_step26579.pt \
+--init=$HOME/remote/yardrat/smart_watch_dvc/training/yardrat/jon.crall/Drop1RawLeftRight/runs/DirectCD_smt_it_joint_p8_raw7common_v4/lightning_logs/version_2/checkpoints/epoch=2-step=1241-v3.ckpt \
+
+
