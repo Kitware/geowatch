3faf6449508a5a53dea53c02180918cc247c6983
joncrall <erotemic@gmail.com>
2021-10-18 13:50:33 -0400

    Add monkeypatch file for syspath modification

--- watch/tasks/depth/pl_highres_verify.py
+++ watch/tasks/depth/pl_highres_verify.py
@@ -1,8 +1,8 @@
-
-import os  # NOQA
-import sys  # NOQA
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
+# FIXME:
+# Adds the "modules" subdirectory to the python path.
+# See https://gitlab.kitware.com/smart/watch/-/merge_requests/148#note_1050127
+# for discussion of how to refactor this in the future.
+from . import modules_monkeypatch  # NOQA
 
 import warnings
 import numpy as np
