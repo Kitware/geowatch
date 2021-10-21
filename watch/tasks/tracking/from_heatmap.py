a05eb23c5553d51c53877254b1cba349c72aaa94
Usman Rafique <usman.rafique@kitware.com>
2021-10-21 00:59:47 -0400

    wip

--- watch/tasks/tracking/from_heatmap.py
+++ watch/tasks/tracking/from_heatmap.py
@@ -3,7 +3,7 @@ from watch.utils import util_kwimage
 import kwarray
 import kwimage
 import numpy as np
-
+import ubelt as ub
 
 def mask_to_scored_polygons(probs, thresh):
     """
@@ -40,14 +40,65 @@ def mask_to_scored_polygons(probs, thresh):
         yield poly, score
 
 
-def time_aggregated_polys(coco_dset, thresh=0.15, morph_kernel=3):
+def time_aggregated_polys2(coco_dset, thresh=0.15, morph_kernel=3):
     '''
     Track function.
 
     Aggregate heatmaps across time, threshold them to get polygons,
     and add one track per polygon.
     '''
-    key = 'change_prob'
+    #key = 'change_prob'
+    key = 'salient'
+    for vidid, video in coco_dset.index.videos.items():
+        print('video', ub.dict_isect(video, ['width', 'height']))
+        running = kwarray.RunningStats()
+        gids = coco_dset.index.vidid_to_gids[vidid]
+        for gid in gids:
+            img = coco_dset.index.imgs[gid]
+            coco_img = kwcoco_extensions.CocoImage(img, coco_dset)
+            if key in coco_img.channels:
+                img_probs = coco_img.delay(key, space='video').finalize()
+                running.update(img_probs)
+
+        probs = running.summarize(axis=2, keepdims=False)['mean']
+
+        hard_probs = util_kwimage.morphology(probs > thresh, 'close', morph_kernel)
+        modulated_probs = probs * hard_probs
+
+        scored_polys = list(mask_to_scored_polygons(modulated_probs, thresh))
+
+        print('number of polygons:', len(scored_polys))
+        # Add each polygon to every images as a track
+        new_trackids = kwcoco_extensions.TrackidGenerator(coco_dset)
+        change_cid = coco_dset.ensure_category(key)
+        for vid_poly, score in scored_polys:
+            score=1.0
+            track_id = next(new_trackids)
+            for gid, img in coco_dset.imgs.items():
+                vid_from_img = kwimage.Affine.coerce(img['warp_img_to_vid'])
+                img_from_vid = vid_from_img.inv()
+
+                # Transform the video polygon into image space
+                img_poly = vid_poly.warp(img_from_vid)
+                bbox = list(img_poly.bounding_box().to_coco())[0]
+                # Add the polygon as an annotation on the image
+                coco_dset.add_annotation(
+                    image_id=gid, category_id=change_cid,
+                    bbox=bbox, segmentation=img_poly, score=score,
+                    track_id=track_id)
+
+    return coco_dset
+
+
+def time_aggregated_polys(coco_dset, thresh=0.3, morph_kernel=3):
+    '''
+    Track function.
+
+    Aggregate heatmaps across time, threshold them to get polygons,
+    and add one track per polygon.
+    '''
+    #key = 'change_prob'
+    key = 'salient'
     running = kwarray.RunningStats()
     for img in coco_dset.imgs.values():
         coco_img = coco_dset.coco_image(img['id'])
@@ -62,11 +113,12 @@ def time_aggregated_polys(coco_dset, thresh=0.15, morph_kernel=3):
 
     scored_polys = list(mask_to_scored_polygons(modulated_probs, thresh))
 
-    # print('number of polygons:', len(scored_polys))
+    print('number of polygons:', len(scored_polys))
     # Add each polygon to every images as a track
     new_trackids = kwcoco_extensions.TrackidGenerator(coco_dset)
     change_cid = coco_dset.ensure_category(key)
     for vid_poly, score in scored_polys:
+        score=1.0
         track_id = next(new_trackids)
         for gid, img in coco_dset.imgs.items():
             vid_from_img = kwimage.Affine.coerce(img['warp_img_to_vid'])
