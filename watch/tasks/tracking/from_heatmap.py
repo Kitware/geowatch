1163c75a7dabc1e7e44952955ccfd2e243263086
Matthew <matthew.bernstein@kitware.com>
2021-10-27 18:36:43 -0400

    wip

--- watch/tasks/tracking/from_heatmap.py
+++ watch/tasks/tracking/from_heatmap.py
@@ -1,8 +1,26 @@
 from watch.utils import kwcoco_extensions
 from watch.utils import util_kwimage
+from collections import defaultdict
 import kwarray
 import kwimage
 import numpy as np
+
+
+def _score(poly, probs):
+    # Compute a score for the polygon
+    # First compute the valid bounds of the polygon
+    # And create a mask for only the valid region of the polygon
+    box = poly.bounding_box().quantize().to_xywh()
+    # Ensure w/h are positive
+    box.data[:, 2:4] = np.maximum(box.data[:, 2:4], 1)
+    x, y, w, h = box.data[0]
+    rel_poly = poly.translate((-x, -y))
+    rel_mask = rel_poly.to_mask((h, w)).data
+    # Slice out the corresponding region of probabilities
+    rel_probs = probs[y:y + h, x:x + w]
+    total = rel_mask.sum()
+    score = 0 if total == 0 else (rel_mask * rel_probs).sum() / total
+    return score
 
 
 def mask_to_scored_polygons(probs, thresh):
@@ -24,50 +42,125 @@ def mask_to_scored_polygons(probs, thresh):
     hard_mask = kwimage.Mask(hard_mask_c, 'c_mask')
     polygons = hard_mask.to_multi_polygon()
     for poly in polygons:
-        # Compute a score for the polygon
-        # First compute the valid bounds of the polygon
-        # And create a mask for only the valid region of the polygon
-        box = poly.bounding_box().quantize().to_xywh()
-        # Ensure w/h are positive
-        box.data[:, 2:4] = np.maximum(box.data[:, 2:4], 1)
-        x, y, w, h = box.data[0]
-        rel_poly = poly.translate((-x, -y))
-        rel_mask = rel_poly.to_mask((h, w)).data
-        # Slice out the corresponding region of probabilities
-        rel_probs = probs[y:y + h, x:x + w]
-        total = rel_mask.sum()
-        score = 0 if total == 0 else (rel_mask * rel_probs).sum() / total
-        yield poly, score
+        yield poly, _score(poly, probs)
 
 
-def time_aggregated_polys(coco_dset, thresh=0.15, morph_kernel=3, key='salient'):
+def time_aggregated_polys(coco_dset,
+                          thresh=0.15,
+                          morph_kernel=3,
+                          key='salient',
+                          bg_key=None):
     '''
     Track function.
 
     Aggregate heatmaps across time, threshold them to get polygons,
     and add one track per polygon.
+
+    Args:
+        key (String | List[String]): foreground key(s).
+
+        bg_key (String | List[String] | None): background key(s).
+            If None, background heatmaps become 1 - sum(foreground keys)
+
+        thresh (float): For each frame, if sum of foreground heatmaps > thresh,
+            class is max(foreground keys).
+            else, class is max(background keys).
+
+        morph_kernel (int): height/width in px of close-kernel
     '''
-    running = kwarray.RunningStats()
+
+    # for backwards compatibility
+    if isinstance(key, str):
+        key = [key]
+    if bg_key is None:
+        bg_key = []
+    elif isinstance(bg_key, str):
+        bg_key = [bg_key]
+
+    # error checking
+    if len(key) < 1:
+        raise ValueError('must have at least one key')
+    if (len(key) > len(set(key)) or len(bg_key) > len(set(bg_key))):
+        raise ValueError('keys are duplicated')
+    if not set(key).isdisjoint(set(bg_key)):
+        raise ValueError('cannot have a key in foreground and background')
+    _all_keys = set(key + bg_key)
+    if all(
+            _all_keys.disjoint(
+                kwcoco_extensions.CocoImage(img, coco_dset).channels)
+            for img in coco_dset.imgs.values()):
+        raise ValueError(f'{coco_dset.tag} has no keys {key} or {bg_key}')
+
+    # record fg and bg keys across frames, and partial sums of fg and bg
+    # this guarantees RunningStats of equal length for all keys,
+    # even with partial/nonexistence
+    running_dct = defaultdict(kwarray.RunningStats)
     for img in coco_dset.imgs.values():
+
         coco_img = coco_dset.coco_image(img['id'])
-        if key in coco_img.channels:
-            img_probs = coco_img.delay(key, space='video').finalize()
-            running.update(img_probs)
+        zeros = np.zeros_like(
+            coco_img.delay(coco_img.channels[0], space='video').finalize())
+        fg_img_probs = zeros
+        bg_img_probs = zeros
 
-    probs = running.summarize(axis=2, keepdims=False)['mean']
+        for k in key:
+            if k in coco_img.channels:
+                img_probs = coco_img.delay(key, space='video').finalize()
+                fg_img_probs += img_probs
+                running_dct[k].update(img_probs)
+            else:
+                running_dct[k].update(zeros)
+        running_dct['fg'].update(fg_img_probs)
 
-    hard_probs = util_kwimage.morphology(probs > thresh, 'close', morph_kernel)
-    modulated_probs = probs * hard_probs
+        for k in bg_key:
+            if k in coco_img.channels:
+                img_probs = coco_img.delay(key, space='video').finalize()
+                bg_img_probs += img_probs
+                running_dct[k].update(img_probs)
+            else:
+                running_dct[k].update(zeros)
+        running_dct['bg'].update(bg_img_probs)
 
-    scored_polys = list(mask_to_scored_polygons(modulated_probs, thresh))
+    # turn heatmaps into scores and polygons
+    def probs(running):
+        probs = running.summarize(axis=2, keepdims=False)['mean']
+
+        hard_probs = util_kwimage.morphology(probs > thresh, 'close',
+                                             morph_kernel)
+        modulated_probs = probs * hard_probs
+        return modulated_probs
+
+    # TODO this still restricts to same-shape polygon in every frame
+    # only the label (key) changes per-frame
+    # to generalize this, have to get scored_polys from all keys
+    # and associate them somehow
+    scored_polys = list(
+        mask_to_scored_polygons(probs(running_dct['fg']), thresh))
 
     print('time aggregation: number of polygons:', len(scored_polys))
     # Add each polygon to every images as a track
     new_trackids = kwcoco_extensions.TrackidGenerator(coco_dset)
-    change_cid = coco_dset.ensure_category(key)
+
     for vid_poly, score in scored_polys:
         track_id = next(new_trackids)
         for gid, img in coco_dset.imgs.items():
+
+            # assign category (key) from max score
+            coco_img = kwcoco_extensions.CocoImage(img, coco_dset)
+            if score > thresh or len(bg_key) == 0:
+                cand_keys = key
+            else:
+                cand_keys = bg_key
+            cand_scores = []
+            for k in cand_keys:
+                if k in coco_img.channels:
+                    img_probs = coco_img.delay(key, space='video').finalize()
+                    cand_scores.append(_score(vid_poly, img_probs))
+                else:
+                    cand_scores.append(0)
+            cat_name = np.max(cand_keys, cand_scores)
+            cid = coco_dset.ensure_category(cat_name)
+
             vid_from_img = kwimage.Affine.coerce(img['warp_img_to_vid'])
             img_from_vid = vid_from_img.inv()
 
@@ -75,9 +168,60 @@ def time_aggregated_polys(coco_dset, thresh=0.15, morph_kernel=3, key='salient')
             img_poly = vid_poly.warp(img_from_vid)
             bbox = list(img_poly.bounding_box().to_coco())[0]
             # Add the polygon as an annotation on the image
-            coco_dset.add_annotation(
-                image_id=gid, category_id=change_cid,
-                bbox=bbox, segmentation=img_poly, score=score,
-                track_id=track_id)
+            coco_dset.add_annotation(image_id=gid,
+                                     category_id=cid,
+                                     bbox=bbox,
+                                     segmentation=img_poly,
+                                     score=score,
+                                     track_id=track_id)
 
     return coco_dset
+
+
+def time_aggregated_polys_bas(coco_dset, thresh=0.15, morph_kernel=3):
+    '''
+    Wrapper for BAS that looks for change heatmaps.
+    '''
+    change_keys = ['salient', 'change_prob', 'change']
+    for change_key in change_keys:
+        try:
+            return time_aggregated_polys(coco_dset, thresh, morph_kernel,
+                                         change_key)
+        except ValueError:
+            pass
+
+    raise ValueError(
+        f'{coco_dset.tag} does not contain any image channel {change_keys}')
+
+
+def time_aggregated_polys_sc(coco_dset,
+                             thresh=0.15,
+                             morph_kernel=3,
+                             bg_thresh=True):
+    '''
+    Wrapper for Site Characterization that looks for phase heatmaps.
+
+    Args:
+        bg_thresh: If True, treat No Activity as background and other classes as foreground,
+            using thresh to switch between them. Else, all classes are foreground.
+            TODO enable using a different thresh here than in mask_to_scored_polygons
+    '''
+    if bg_thresh:
+        return time_aggregated_polys(coco_dset,
+                                     thresh,
+                                     morph_kernel,
+                                     key=[
+                                         'Site Preparation',
+                                         'Active Construction',
+                                         'Post Construction'
+                                     ],
+                                     bg_key=['No Activity'])
+    else:
+        return time_aggregated_polys(coco_dset,
+                                     thresh,
+                                     morph_kernel,
+                                     key=[
+                                         'Site Preparation',
+                                         'Active Construction',
+                                         'Post Construction', 'No Activity'
+                                     ])
