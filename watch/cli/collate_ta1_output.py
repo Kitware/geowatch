a7f78f4f1086de3d1e1d1a20c4b39cc08ebf957d
David Joy <david.joy@kitware.com>
2021-11-04 22:39:23 -0400

    TA-1 output collation script, first pass

---
+++ watch/cli/collate_ta1_output.py
@@ -0,0 +1,416 @@
+import argparse
+import sys
+import json
+import os
+import tempfile
+import subprocess
+from dateutil.parser import parse
+from concurrent.futures import as_completed
+import re
+from functools import partial
+
+import ubelt as ub
+import pystac
+from osgeo import gdal
+
+from watch.utils import util_bands
+
+
+SUPPORTED_S2_PLATFORMS = {'S2A',
+                          'S2B',
+                          'sentinel-2a',
+                          'sentinel-2b'}  # Sentinel
+SUPPORTED_LS_PLATFORMS = {'OLI_TIRS',
+                          'LANDSAT_8'}  # Landsat
+SUPPORTED_WV_PLATFORMS = {'DigitalGlobe'}  # Worldview
+SUPPORTED_PLATFORMS = (SUPPORTED_S2_PLATFORMS |
+                       SUPPORTED_LS_PLATFORMS |
+                       SUPPORTED_WV_PLATFORMS)
+PLATFORM_SHORTHAND = {**{p: 's2' for p in SUPPORTED_S2_PLATFORMS},
+                      **{p: 'ls' for p in SUPPORTED_LS_PLATFORMS},
+                      **{p: 'wv' for p in SUPPORTED_WV_PLATFORMS}}
+
+# Maps Sentinel 2 STAC item asset names to the suffixes they should
+# use in the collated output.  Note that this also serves as a filter
+# as any asset name not found here will be excluded from the output
+S2_ASSET_NAME_MAP = {'image-B01': 'B01',
+                     'image-B02': 'B02',
+                     'image-B03': 'B03',
+                     'image-B04': 'B04',
+                     'image-B05': 'B05',
+                     'image-B06': 'B06',
+                     'image-B07': 'B07',
+                     'image-B08': 'B08',
+                     'image-B09': 'B09',
+                     'image-B10': 'B10',
+                     'image-B11': 'B11',
+                     'image-B12': 'B12',
+                     'image-B8A': 'B8A',
+                     'image-cloudmask': 'QA'}
+
+# Maps Landsat 8 STAC item asset names to the suffixes they should
+# use in the collated output.  Note that this also serves as a filter
+# as any asset name not found here will be excluded from the output
+L8_ASSET_NAME_MAP = {'image-B1': 'B01',
+                     'image-B2': 'B02',
+                     'image-B3': 'B03',
+                     'image-B4': 'B04',
+                     'image-B5': 'B05',
+                     'image-B6': 'B06',
+                     'image-B7': 'B07',
+                     'image-B8': 'B08',
+                     'image-B9': 'B09',
+                     'image-B10': 'B10',
+                     'image-B11': 'B11',
+                     'image-cloudmask': 'QA'}
+
+# Helper map to take asset suffixes (if different) from maps above to
+# asset names as they should appear in the output STAC items
+ASSET_SUFFIX_TO_NAME_MAP = {'QA': 'quality',
+                            'TCI': 'visual'}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Collate TA-1 output data for T&E consumption")
+    parser.add_argument('input_path',
+                        type=str,
+                        help="Path to input T&E Baseline Framework JSON")
+    parser.add_argument('output_bucket',
+                        type=str,
+                        help="S3 bucket path for collated data")
+    parser.add_argument("--aws_profile",
+                        required=False,
+                        type=str,
+                        help="AWS Profile to use for AWS S3 CLI commands")
+    parser.add_argument("-d", "--dryrun",
+                        action='store_true',
+                        default=False,
+                        help="Run AWS CLI commands with --dryrun flag")
+    parser.add_argument("--performer_code",
+                        default='kit',
+                        type=str,
+                        help="Performer code suffix for output "
+                             "directories / files (default: 'kit')")
+    parser.add_argument("--eval_num",
+                        default='1',
+                        type=str,
+                        help="Evaluation number string for building "
+                             "output paths (default: '1')")
+    parser.add_argument("-j", "--jobs",
+                        type=int,
+                        default=1,
+                        required=False,
+                        help="Number of jobs to run in parallel")
+
+    collate_ta1_output(**vars(parser.parse_args()))
+
+    return 0
+
+
+def _reformat_bandname(band):
+    """
+    E.g. convert "B7" to "B07"
+    """
+    m = re.match(r'B(\d)', band)
+    if m is not None:
+        return "B{:0>2}".format(m.group(1))
+    else:
+        return band
+
+
+def _load_input(path):
+    try:
+        with open(path) as f:
+            input_json = json.load(f)
+        return input_json['stac'].get('features', [])
+    # Excepting KeyError here in case of a single line STAC item input
+    except (json.decoder.JSONDecodeError, KeyError):
+        # Support for simple newline separated STAC items
+        with open(path) as f:
+            return [json.loads(line) for line in f]
+
+
+def collate_item(stac_item_dict,
+                 aws_base_command,
+                 output_bucket,
+                 performer_code,
+                 eval_num):
+    stac_item = pystac.Item.from_dict(stac_item_dict)
+
+    platform = stac_item.properties['platform']
+
+    if platform not in SUPPORTED_PLATFORMS:
+        print("* Warning unknown platform: '{}' for item, "
+              "skipping!".format(platform))
+        return stac_item
+
+    output_stac_collection_id = 'ta1-{}-{}'.format(
+        PLATFORM_SHORTHAND[platform], performer_code)
+
+    if platform in SUPPORTED_LS_PLATFORMS:
+        original_id = stac_item.properties.get(
+            'landsat:scene_id', stac_item.id)
+    elif platform in SUPPORTED_S2_PLATFORMS:
+        original_id = stac_item.properties.get(
+            'sentinel:product_id', stac_item.id)
+    elif platform in SUPPORTED_WV_PLATFORMS:
+        if 'nitf:auxiliary_image_identifier' in stac_item.properties:
+            original_id, *_ = stac_item.properties.get(
+                'nitf:auxiliary_image_identifier').split()
+        else:
+            original_id = stac_item.id
+
+    item_datetime = parse(stac_item.properties['datetime'])
+    # NOTE ** Assumes that we're compliant with the MGRS STAC
+    # extension (but use dummy values just in case):
+    # https://github.com/stac-extensions/mgrs
+    item_s3_outdir = '/'.join((
+        output_bucket,
+        output_stac_collection_id,
+        str(stac_item.properties.get('mgrs:utm_zone', 'ZZ')),
+        str(stac_item.properties.get('mgrs:latitude_band', 'B')),
+        str(stac_item.properties.get('mgrs:grid_square', 'SS')),
+        "{:0>4}".format(item_datetime.year),
+        "{:0>2}".format(item_datetime.month),
+        "{:0>2}".format(item_datetime.day),
+        "{}_{}".format(original_id, performer_code)))
+
+    eval_name = 'eval-{}'.format(eval_num)
+    ssh_outdir = '/'.join(
+        (output_bucket, eval_name))
+
+    if platform in SUPPORTED_LS_PLATFORMS:
+        platform_collation_fn = partial(generic_collate_item,
+                                        L8_ASSET_NAME_MAP)
+    elif platform in SUPPORTED_S2_PLATFORMS:
+        platform_collation_fn = partial(generic_collate_item,
+                                        S2_ASSET_NAME_MAP)
+    elif platform in SUPPORTED_WV_PLATFORMS:
+        platform_collation_fn = collate_wv_item
+
+    output_stac_item = platform_collation_fn(stac_item,
+                                             aws_base_command,
+                                             original_id,
+                                             item_s3_outdir,
+                                             ssh_outdir,
+                                             performer_code)
+
+    # Completely discard item if platform collation fails
+    if output_stac_item is None:
+        return None
+
+    stac_item_outpath = '/'.join((item_s3_outdir,
+                                  '{}.json'.format(original_id)))
+    output_stac_item.set_self_href(stac_item_outpath)
+
+    original_links = output_stac_item.get_links('original')
+    if len(original_links) > 0:
+        original_stac_item_uri = original_links[0].get_absolute_href()
+    else:
+        original_stac_item_uri = ''
+
+    output_stac_item.properties['smart:performer'] = performer_code
+    output_stac_item.properties['smart:evaluation'] = eval_num
+    output_stac_item.properties['smart:source'] = original_stac_item_uri
+    output_stac_item.collection_id = output_stac_collection_id
+
+    return output_stac_item
+
+
+def generic_collate_item(asset_name_map,
+                         stac_item,
+                         aws_base_command,
+                         original_id,
+                         item_outdir,
+                         ssh_outdir,
+                         performer_code):
+    item_outdir_base = os.path.basename(item_outdir)
+    output_assets = {}
+    for asset_name, asset in stac_item.assets.items():
+        # Don't output asset if not included in map
+        asset_suffix = L8_ASSET_NAME_MAP.get(asset_name)
+
+        if asset_suffix is None:
+            continue
+
+        stac_asset_outpath_basename = "{}_{}_{}.tif".format(
+            original_id, performer_code, asset_suffix)
+        stac_asset_outpath = '/'.join(
+            (item_outdir, stac_asset_outpath_basename))
+
+        # Default to asset_suffix if a map isn't found
+        output_asset_name = ASSET_SUFFIX_TO_NAME_MAP.get(
+            asset_suffix, asset_suffix)
+        output_assets[output_asset_name] = pystac.Asset.from_dict(
+            {'href': stac_asset_outpath,
+             'title': '/'.join((item_outdir_base,
+                                stac_asset_outpath_basename)),
+             'roles': ['data']})
+
+        ssh_asset_outpath = '/'.join(
+            (ssh_outdir, "{}_{}_SSH_{}.tif".format(
+                original_id, performer_code, asset_suffix)))
+
+        # Copy assets up to S3
+        subprocess.run([*aws_base_command,
+                        asset.href, stac_asset_outpath], check=True)
+        subprocess.run([*aws_base_command,
+                        asset.href, ssh_asset_outpath], check=True)
+
+    stac_item.assets = output_assets
+
+    return stac_item
+
+
+def collate_wv_item(stac_item,
+                    aws_base_command,
+                    original_id,
+                    item_outdir,
+                    ssh_outdir,
+                    performer_code):
+    # WV items only have a single "data" asset containing all bands
+    data_asset = stac_item.assets.get('data')
+    if data_asset is None:
+        print("** Error ** Missing expected 'data' asset from "
+              "Worldview STAC Item skipping!")
+        return None
+
+    def _out_bands(band_dicts):
+        return [_reformat_bandname(b['name'])
+                for b in band_dicts]
+
+    with tempfile.NamedTemporaryFile(suffix='.tif') as temp_src_file:
+        subprocess.run([*aws_base_command,
+                        data_asset.href, temp_src_file.name], check=True)
+
+        bands = gdal.Info(temp_src_file.name, format='json')['bands']
+
+        if len(bands) == 1:
+            output_bands = _out_bands(util_bands.WORLDVIEW2_PAN)
+        elif len(bands) == 4:
+            output_bands = _out_bands(util_bands.WORLDVIEW2_MS4)
+        elif len(bands) == 8:
+            output_bands = _out_bands(util_bands.WORLDVIEW2_MS8)
+        else:
+            print('unknown channel signature for WV')
+            return None
+
+        item_outdir_base = os.path.basename(item_outdir)
+        output_assets = {}
+        for band_i, asset_suffix in enumerate(output_bands, start=1):
+            with tempfile.NamedTemporaryFile(suffix='.tif') as temporary_file:
+                # Extract band as a seperate image
+                subprocess.run(['gdal_calc.py',
+                                '--calc', 'A',
+                                '--outfile', temporary_file.name,
+                                '-A', temp_src_file.name,
+                                '--A_band', str(band_i)], check=True)
+
+                stac_asset_outpath_basename = "{}_{}_{}.tif".format(
+                    original_id, performer_code, asset_suffix)
+                stac_asset_outpath = '/'.join(
+                    (item_outdir, stac_asset_outpath_basename))
+
+                # Default to asset_suffix if a map isn't found
+                output_asset_name = ASSET_SUFFIX_TO_NAME_MAP.get(
+                    asset_suffix, asset_suffix)
+                output_assets[output_asset_name] = pystac.Asset.from_dict(
+                    {'href': stac_asset_outpath,
+                     'title': '/'.join((item_outdir_base,
+                                        stac_asset_outpath_basename)),
+                     'roles': ['data']})
+
+                ssh_asset_outpath = '/'.join((
+                    ssh_outdir, "{}_{}_SSH_{}.tif".format(
+                        original_id, performer_code, asset_suffix)))
+
+                # Copy assets up to S3
+                subprocess.run([*aws_base_command,
+                                temporary_file.name, stac_asset_outpath],
+                               check=True)
+                subprocess.run([*aws_base_command,
+                                temporary_file.name, ssh_asset_outpath],
+                               check=True)
+
+    stac_item.assets = output_assets
+
+    return stac_item
+
+
+def collate_ta1_output(input_path,
+                       output_bucket,
+                       aws_profile=None,
+                       dryrun=False,
+                       performer_code='kit',
+                       eval_num='1',
+                       jobs=1):
+    if aws_profile is not None:
+        aws_base_command =\
+            ['aws', 's3', '--profile', aws_profile, 'cp']
+    else:
+        aws_base_command = ['aws', 's3', 'cp']
+
+    if dryrun:
+        aws_base_command.append('--dryrun')
+
+    if input_path.startswith('s3'):
+        with tempfile.NamedTemporaryFile() as temporary_file:
+            subprocess.run(
+                [*aws_base_command, input_path, temporary_file.name],
+                check=True)
+
+            input_stac_items = _load_input(temporary_file.name)
+    else:
+        input_stac_items = _load_input(input_path)
+
+    executor = ub.Executor(mode='process' if jobs > 1 else 'serial',
+                           max_workers=jobs)
+    collation_jobs = [executor.submit(collate_item, stac_item_dict,
+                                      aws_base_command,
+                                      output_bucket,
+                                      performer_code,
+                                      eval_num)
+                      for stac_item_dict in input_stac_items]
+
+    output_stac_items_by_collection = {}
+    for collation_job in ub.ProgIter(as_completed(collation_jobs),
+                                     total=len(collation_jobs),
+                                     desc='collation jobs'):
+        stac_item = collation_job.result()
+        if stac_item is not None:
+            output_stac_items_by_collection.setdefault(
+                stac_item.collection_id, []).append(stac_item)
+
+    for collection_id, stac_items in output_stac_items_by_collection.items():
+        collection_output_path = '/'.join((
+            output_bucket,
+            collection_id,
+            'collection.json'))
+
+        output_collection = pystac.Collection(
+            collection_id,
+            "STAC Collection '{}' for SMART program from team '{}'".format(
+                collection_id, performer_code),
+            pystac.Extent.from_items(stac_items),
+            href=collection_output_path)
+
+        for stac_item in stac_items:
+            output_collection.add_item(stac_item)
+            with tempfile.NamedTemporaryFile() as temporary_file:
+                with open(temporary_file.name, 'w') as f:
+                    json.dump(stac_item.to_dict(), f, indent=2)
+
+                # Assumes that the STAC item's self href has been set
+                # in the per item collation function(s)
+                subprocess.run([*aws_base_command,
+                                temporary_file.name,
+                                stac_item.get_self_href()], check=True)
+
+        with tempfile.NamedTemporaryFile() as temporary_file:
+            with open(temporary_file.name, 'w') as f:
+                json.dump(output_collection.to_dict(), f, indent=2)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
