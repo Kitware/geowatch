5afff5e806f806bc92078b3797af130a525867e3
joncrall <jon.crall@kitware.com>
2021-11-17 13:42:44 -0500

    rename vars

--- scripts/prep_drop1_ta1.sh
+++ scripts/prep_drop1_ta1.sh
@@ -5,17 +5,25 @@ for DVC
 "
 
 # user-specified arguments
-DVC_DPATH=${DVC_DPATH:-$HOME/data/dvc-repos/smart_watch_dvc}
-S3_DPATH=${S3_DPATH:-s3://kitware-smart-watch-data/processed/ta1/drop1/coreg_and_brdf/}
+#DVC_DPATH=${DVC_DPATH:-$HOME/data/dvc-repos/smart_watch_dvc}
+#S3_DPATH=${S3_DPATH:-s3://kitware-smart-watch-data/processed/ta1/drop1/coreg_and_brdf/}
+#S3_DPATH=${S3_DPATH:-s3://kitware-smart-watch-data/processed/ta1/drop1/coreg_and_brdf/}
+DVC_DPATH=$HOME/data/dvc-repos/smart_watch_dvc
+S3_DPATH=s3://kitware-smart-watch-data/processed/ta1/drop1/mtra
+
+TA1_PROCESS_TYPE=MTRA
+
+ALIGNED_BUNDLE_NAME=Drop1-Aligned-TA1-$TA1_PROCESS_TYPE
+UNCROPPED_BUNDLE_NAME=TA1-Uncropped-$TA1_PROCESS_TYPE
 
 
 # The paths we will use to download and store uncropped tiles
-OUTPUT_DPATH=$DVC_DPATH/TA1-Processed
-QUERY_DPATH=$OUTPUT_DPATH/_query/items
-INGRESS_DPATH=$OUTPUT_DPATH/ingress
-QUERY_FPATH=$OUTPUT_DPATH/_query/query.json
-CATALOG_FPATH=$INGRESS_DPATH/catalog.json
-UNSTRUCTURED_KWCOCO_FPATH=$OUTPUT_DPATH/data.kwcoco.json
+UNCROPPED_DPATH=$DVC_DPATH/$UNCROPPED_BUNDLE_NAME
+UNCROPPED_QUERY_DPATH=$UNCROPPED_DPATH/_query/items
+UNCROPPED_INGRESS_DPATH=$UNCROPPED_DPATH/ingress
+UNCROPPED_QUERY_FPATH=$UNCROPPED_DPATH/_query/query.json
+UNCROPPED_CATALOG_FPATH=$UNCROPPED_INGRESS_DPATH/catalog.json
+UNCROPPED_KWCOCO_FPATH=$UNCROPPED_DPATH/data.kwcoco.json
 
 
 # The file that defines the TA-2 regions to crop to
@@ -24,7 +32,7 @@ REGION_FPATH=$DVC_DPATH/drop1/all_regions.geojson
 
 # The folder that will contain the TA2-ready kwcoco dataset
 # TODO: is the the right name for the new TA2-dataset?
-ALIGNED_KWCOCO_BUNDLE_DPATH=$DVC_DPATH/Drop1-Aligned-TA1-2021-11
+ALIGNED_KWCOCO_BUNDLE_DPATH=$DVC_DPATH/$ALIGNED_BUNDLE_NAME
 ALIGNED_KWCOCO_FPATH=$ALIGNED_KWCOCO_BUNDLE_DPATH/data.kwcoco.json
 
 
@@ -34,30 +42,30 @@ download_uncropped_data(){
     "
 
     # Grab the stac items we will query directly from S3 and combine it into a single query json file
-    mkdir -p $INGRESS_DPATH
-    mkdir -p $QUERY_DPATH
-    aws s3 --profile iarpa sync --exclude '*' --include '*.json' $S3_DPATH $QUERY_DPATH
-    jq . --indent 0 $QUERY_DPATH/*.json > $QUERY_FPATH
+    mkdir -p $UNCROPPED_INGRESS_DPATH
+    mkdir -p $UNCROPPED_QUERY_DPATH
+    aws s3 --profile iarpa sync --exclude '*' --include '*.json' $S3_DPATH $UNCROPPED_QUERY_DPATH
+    jq . --indent 0 $UNCROPPED_QUERY_DPATH/*.json > $UNCROPPED_QUERY_FPATH
 
 
     # Use the watch script to pull down the images as a stac catalog
     python -m watch.cli.baseline_framework_ingress \
         --aws_profile iarpa \
         --jobs 4 \
-        --outdir $INGRESS_DPATH $QUERY_FPATH 
+        --outdir $UNCROPPED_INGRESS_DPATH $UNCROPPED_QUERY_FPATH 
 
 
     # Convert the stac catalog to kwcoco format
     python -m watch.cli.ta1_stac_to_kwcoco \
-        $CATALOG_FPATH \
-        --outpath=$UNSTRUCTURED_KWCOCO_FPATH 
+        $UNCROPPED_CATALOG_FPATH \
+        --outpath=$UNCROPPED_KWCOCO_FPATH 
 
 
     # Preprocess the unstructured kwcoco file to ensure geo-info is in the json
     # (This is optional, but it makes the next step faster)
     python -m watch.cli.coco_add_watch_fields \
-        --src $UNSTRUCTURED_KWCOCO_FPATH \
-        --dst $UNSTRUCTURED_KWCOCO_FPATH \
+        --src $UNCROPPED_KWCOCO_FPATH \
+        --dst $UNCROPPED_KWCOCO_FPATH \
         --overwrite=warp
 }
 
@@ -68,7 +76,7 @@ crop_to_regions(){
     "
     # Crop the unstructured data into "videos" aligned to each region.
     python -m watch.cli.coco_align_geotiffs \
-        --src $UNSTRUCTURED_KWCOCO_FPATH \
+        --src $UNCROPPED_KWCOCO_FPATH \
         --dst $ALIGNED_KWCOCO_FPATH \
         --regions $REGION_FPATH \
         --max_workers=10 \
