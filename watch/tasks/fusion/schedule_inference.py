3bed4a66aa951da425f2336d0f170da9354c56fa
joncrall <jon.crall@kitware.com>
2021-11-22 21:41:54 -0500

    Breakout the poor-mans tmux queue

--- watch/tasks/fusion/schedule_inference.py
+++ watch/tasks/fusion/schedule_inference.py
@@ -3,6 +3,152 @@ Helper for scheduling a set of prediction + evaluation jobs
 
 python -m watch.tasks.fusion.schedule_inference gather_candidate_models
 """
+import pathlib
+import ubelt as ub
+import itertools as it
+import stat
+import os
+
+
+class TMUXQueue(ub.NiceRepr):
+    """
+    A lightweight, but limited job queue
+
+    Example:
+        >>> TMUXQueue('foo', 'foo')
+    """
+    def __init__(self, name, dpath, environ=None):
+        self.name = name
+        self.environ = environ
+        self.dpath = pathlib.Path(dpath)
+        self.fpath = self.dpath / (name + '.sh')
+        self.header = ['#!/bin/bash']
+        self.commands = []
+
+    def __nice__(self):
+        return f'{self.name} - {len(self.commands)}'
+
+    def finalize_text(self):
+        script = self.header
+        if self.environ:
+            script = script + [
+                f'export {k}="{v}"' for k, v in self.environ.items()]
+        script = script + self.commands
+        text = '\n'.join(script)
+        return text
+
+    def submit(self, command):
+        self.commands.append(command)
+
+    def write(self):
+        text = self.finalize_text()
+        with open(self.fpath, 'w') as file:
+            file.write(text)
+        os.chmod(self.fpath, (
+            stat.S_IXUSR | stat.S_IXGRP | stat.S_IRUSR |
+            stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP))
+        return self.fpath
+
+
+class TMUXMultiQueue(ub.NiceRepr):
+    """
+    Create multiple sets of jobs to start in detatched tmux sessions
+
+    Example:
+        >>> from watch.tasks.fusion.schedule_inference import *  # NOQA
+        >>> self = TMUXMultiQueue('foo', 2)
+        >>> print('self = {!r}'.format(self))
+        >>> self.submit('echo hello')
+        >>> self.submit('echo world')
+        >>> self.submit('echo foo')
+        >>> self.submit('echo bar')
+        >>> self.submit('echo bazbiz')
+        >>> self.write()
+        >>> self.rprint()
+
+    """
+    def __init__(self, name, size=1, environ=None, dpath=None, gres=None):
+        if dpath is None:
+            dpath = ub.ensure_app_cache_dir('watch/tmux_queue')
+        self.dpath = pathlib.Path(dpath)
+        self.name = name
+        self.size = size
+        self.environ = environ
+        self.fpath = self.dpath / f'run_queues_{self.name}.sh'
+
+        per_worker_environs = [environ] * size
+        if gres:
+            # TODO: more sophisticated GPU policy?
+            per_worker_environs = [
+                ub.dict_union(e, {
+                    'CUDA_VISIBLE_DEVICES': str(cvd),
+                })
+                for cvd, e in zip(gres, per_worker_environs)]
+
+        self.workers = [
+            TMUXQueue(
+                name='queue_{}_{}'.format(self.name, worker_idx),
+                dpath=self.dpath,
+                environ=e
+            )
+            for worker_idx, e in enumerate(per_worker_environs)
+        ]
+        self._worker_cycle = it.cycle(self.workers)
+
+    def __nice__(self):
+        return ub.repr2(self.workers)
+
+    def __iter__(self):
+        yield from self._worker_cycle
+
+    def submit(self, command):
+        return next(self._worker_cycle).submit(command)
+
+    def finalize_text(self):
+        # Create a driver script
+        driver_lines = [ub.codeblock(
+            '''
+            #!/bin/bash
+            # Driver script to start the tmux-queue
+            echo "submitting jobs"
+            ''')]
+        for queue in self.workers:
+            # run_command_in_tmux_queue(command, name)
+            part = ub.codeblock(
+                f'''
+                ### Run Queue: {queue.name}
+                tmux new-session -d -s {queue.name} "bash"
+                tmux send -t {queue.name} "source {queue.fpath}" Enter
+                ''').format()
+            driver_lines.append(part)
+        driver_lines += ['echo "jobs submitted"']
+        driver_text = '\n\n'.join(driver_lines)
+        return driver_text
+
+    def write(self):
+        text = self.finalize_text()
+        for queue in self.workers:
+            queue.write()
+        with open(self.fpath, 'w') as file:
+            file.write(text)
+        os.chmod(self.fpath, (
+            stat.S_IXUSR | stat.S_IXGRP | stat.S_IRUSR |
+            stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP))
+        return self.fpath
+
+    def run(self):
+        return ub.cmd(f'bash {self.fpath}', verbose=3, check=True)
+
+    def rprint(self):
+        from rich.panel import Panel
+        from rich.syntax import Syntax
+        from rich.console import Console
+        console = Console()
+        for queue in self.workers:
+            code = queue.finalize_text()
+            console.print(Panel(Syntax(code, 'bash'), title=str(queue.fpath)))
+        code = self.finalize_text()
+        console.print(Panel(Syntax(code, 'bash'), title=str(self.fpath)))
 
 
 def gather_candidate_models():
@@ -61,33 +207,13 @@ def gather_candidate_models():
     # GPUS = [0, 1, 2, 3]
     GPUS = [0]
 
-    parallel_job_ids = range(len(GPUS))
-    parallel_queues = {
-        queue_index: {
-            'name': 'queue_{}_{}'.format(stamp, queue_index),
-            'index': queue_index,
-            'gpu_index': GPUS[queue_index],
-            'commands': [],
-        }
-        for queue_index in parallel_job_ids
+    environ = {
+        'DVC_DPATH': dvc_dpath,
     }
 
-    for queue in parallel_queues.values():
-        queue['fpath'] = tmux_schedule_dpath / (queue['name'] + '.sh')
-        queue['commands'].append(
-            '#!/bin/bash'
-        )
-        queue['commands'].append(
-            'export DVC_DPATH="{}"'.format(str(dvc_dpath))
-        )
-        queue['commands'].append(
-            'export CUDA_VISIBLE_DEVICES={}'.format(str(queue['gpu_index']))
-        )
-    import itertools as it
-    script_cycle = it.cycle(parallel_queues.values())
-    for idx, info in enumerate(packages_to_eval):
-        script = next(script_cycle)
-
+    jobs = TMUXMultiQueue(name=stamp, size=len(GPUS), environ=environ,
+                          gres=GPUS, dpath=tmux_schedule_dpath)
+    for info, queue in zip(packages_to_eval, jobs):
         package_fpath = info['fpath']
         suggestions = organize.suggest_paths(package_fpath=package_fpath, test_dataset=test_dataset_fpath)
         suggestions = json.loads(suggestions)
@@ -113,7 +239,7 @@ def gather_candidate_models():
                     --gpus=0 \
                     --batch_size=1
                 ''').format(**suggestions).replace(str(dvc_dpath), '$DVC_DPATH')
-            script['commands'].append(pred_command)
+            queue.submit(pred_command)
 
         if with_eval:
             eval_command = ub.codeblock(
@@ -123,47 +249,11 @@ def gather_candidate_models():
                         --pred_dataset={pred_dataset} \
                           --eval_dpath={eval_dpath}
                 ''').format(**suggestions).replace(str(dvc_dpath), '$DVC_DPATH')
-            script['commands'].append(eval_command)
+            queue.submit(eval_command)
 
-    import stat
-    import os
-    for queue in parallel_queues.values():
-        fpath = queue['fpath']
-        text = '\n\n'.join(queue['commands'])
-        with open(fpath, 'w') as file:
-            file.write(text)
-        print(text)
-        os.chmod(fpath, stat.S_IXUSR | stat.S_IXGRP | stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
+    jobs.rprint()
 
-    # Create a driver script
-
-    # Start the jobs in a detatched temux session
-    driver_lines = ['#!/bin/bash']
-    driver_lines += ['# Driver script to start the tmux-queue']
-    driver_lines += ['echo "submitting jobs"']
-    import stat
-    import os
-    for queue in parallel_queues.values():
-        fpath = str(queue['fpath'])
-        name = str(queue['name'])
-        command = 'source {}'.format(fpath)
-        # run_command_in_tmux_queue(command, name)
-        part = ub.codeblock(
-            '''
-            ### Run Queue: {name}
-            tmux new-session -d -s {name} "bash"
-            tmux send -t {name} "{command}" Enter
-            ''').format(name=name, command=command)
-        driver_lines.append(part)
-    driver_lines += ['echo "jobs submitted"']
-
-    driver_text = '\n\n'.join(driver_lines)
-    driver_fpath = tmux_schedule_dpath / 'run_queues_{}.sh'.format(stamp)
-    with open(driver_fpath, 'w') as file:
-        file.write(driver_text)
-    os.chmod(driver_fpath, stat.S_IXUSR | stat.S_IXGRP | stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP)
-    print('driver_fpath = {!r}'.format(driver_fpath))
-
+    driver_fpath = jobs.write()
     # RUN
     ub.cmd('bash ' + str(driver_fpath), verbose=3, check=True)
 
