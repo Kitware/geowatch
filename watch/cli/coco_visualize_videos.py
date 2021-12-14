Fix normalization wrt min/max values; misc fixes for new scaled data

watch/cli/coco_visualize_videos.py
@@ -81,6 +81,8 @@ class CocoVisualizeConfig(scfg.Config):
 
         'norm_hack': scfg.Value(False, help='if true apply normalization hack'),
 
+        'nodata': scfg.Value(0, type=int, help='Nodata value'),
+
         'extra_header': scfg.Value(None, help='extra text to include in the header'),
 
         'select_images': scfg.Value(
@@ -282,7 +284,8 @@ def main(cmdline=True, **kwargs):
                                 channels=channels, vid_crop_box=vid_crop_box,
                                 _header_extra=_header_extra,
                                 chan_to_normalizer=chan_to_normalizer,
-                                norm_hack=config['norm_hack'])
+                                norm_hack=config['norm_hack'],
+                                nodata=config['nodata'])
 
         else:
             gid_subset = gids[start_frame:end_frame]
@@ -301,7 +304,8 @@ def main(cmdline=True, **kwargs):
                             draw_imgs=config['draw_imgs'],
                             draw_anns=config['draw_anns'], _header_extra=_header_extra,
                             chan_to_normalizer=chan_to_normalizer,
-                            norm_hack=config['norm_hack'])
+                            norm_hack=config['norm_hack'],
+                            nodata=config['nodata'])
 
         for job in ub.ProgIter(pool.as_completed(), total=len(pool), desc='write imgs'):
             job.result()
@@ -363,7 +367,8 @@ def _write_ann_visualizations2(coco_dset : kwcoco.CocoDataset,
                                request_grouped_bands='default',
                                draw_imgs=True,
                                draw_anns=True, _header_extra=None,
-                               chan_to_normalizer=None, norm_hack=False):
+                               chan_to_normalizer=None, norm_hack=False,
+                               nodata=0):
     """
     Dumps an intensity normalized "space-aligned" kwcoco image visualization
     (with or without annotation overlays) for specific bands to disk.
@@ -517,12 +522,15 @@ def _write_ann_visualizations2(coco_dset : kwcoco.CocoDataset,
             from kwcoco.util import util_delayed_poc
             chan = util_delayed_poc.DelayedChannelConcat([delayed]).take_channels(chan_group)
 
-        canvas = chan.finalize()
+        # Note: Using 'nearest' here since we're just visualizing (and
+        # otherwise nodata values can affect interpolated pixel
+        # values)
+        canvas = chan.finalize(interpolation='nearest')
         # import kwarray
         # kwarray.atleast_nd(canvas, 3)
 
         if chan_to_normalizer is None:
-            canvas = normalize_intensity(canvas, nodata=0, params={
+            canvas = normalize_intensity(canvas, nodata=nodata, params={
                 'high': 0.90,
                 'mid': 0.5,
                 'low': 0.01,
@@ -535,8 +543,8 @@ def _write_ann_visualizations2(coco_dset : kwcoco.CocoDataset,
             for cx, c in enumerate(chan_list):
                 normalizer = chan_to_normalizer[c]
                 data = canvas[..., cx]
-                mask = (data != 0)
-                p = util_kwarray.apply_normalizer(data, normalizer, mask=mask)
+                mask = (data != nodata)
+                p = util_kwarray.apply_normalizer(data, normalizer, mask=mask, set_value_at_mask=0.)
                 new_parts.append(p)
             canvas = np.stack(new_parts, axis=2)
 

watch/utils/util_kwarray.py
@@ -5,6 +5,13 @@ import numpy as np
 import math
 import ubelt as ub
 
+try:
+    # The math variant only exists in Python 3+ but is faster for scalars
+    # so try and use it
+    from math import isclose
+except Exception:
+    from numpy import isclose
+
 
 def cartesian_product(*arrays):
     """
@@ -342,8 +349,7 @@ def _tukey_quantile_extreme_estimator(data):
     return fence_lower, q2, fence_upper
 
 
-def apply_normalizer(data, normalizer, mask=None):
-    import kwarray
+def apply_normalizer(data, normalizer, mask=None, set_value_at_mask=float('nan')):
     dtype = np.float32
     result = data.astype(dtype).copy()
 
@@ -354,19 +360,220 @@ def apply_normalizer(data, normalizer, mask=None):
             valid_data = result[mask]
         else:
             valid_data = result
+
         if valid_data.size > 0:
-            data_normalized = kwarray.normalize(
+            data_normalized = normalize(
                 valid_data.astype(dtype), mode=normalizer['mode'],
-                beta=normalizer['beta'], alpha=normalizer['alpha'],
+                beta=normalizer.get('beta'), alpha=normalizer.get('alpha'),
+                min_val=normalizer.get('min_val'),
+                max_val=normalizer.get('max_val')
             )
         else:
             data_normalized = valid_data
+
     if mask is not None:
         mask_flat = mask.ravel()
         result_flat = result.ravel()
         result_flat[mask_flat] = data_normalized
-        # result[mask] =
-        # result = np.where(mask, data_normalized, data)
+        result_flat[~mask_flat] = set_value_at_mask
     else:
         result = data_normalized
     return result
+
+
+def normalize(arr, mode='linear', alpha=None, beta=None, out=None,
+              min_val=None, max_val=None):
+    """
+    Rebalance signal values via contrast stretching.
+
+    By default linearly stretches array values to minimum and maximum values.
+
+    Args:
+        arr (ndarray): array to normalize, usually an image
+
+        out (ndarray | None): output array. Note, that we will create an
+            internal floating point copy for integer computations.
+
+        mode (str): either linear or sigmoid.
+
+        alpha (float): Only used if mode=sigmoid.  Division factor
+            (pre-sigmoid). If unspecified computed as:
+            ``max(abs(old_min - beta), abs(old_max - beta)) / 6.212606``.
+            Note this parameter is sensitive to if the input is a float or
+            uint8 image.
+
+        beta (float): subtractive factor (pre-sigmoid). This should be the
+            intensity of the most interesting bits of the image, i.e. bring
+            them to the center (0) of the distribution.
+            Defaults to ``(max - min) / 2``.  Note this parameter is sensitive
+            to if the input is a float or uint8 image.
+
+        min_val: override minimum value
+
+        max_val: override maximum value
+
+    References:
+        https://en.wikipedia.org/wiki/Normalization_(image_processing)
+
+    Example:
+        >>> raw_f = np.random.rand(8, 8)
+        >>> norm_f = normalize(raw_f)
+
+        >>> raw_f = np.random.rand(8, 8) * 100
+        >>> norm_f = normalize(raw_f)
+        >>> assert isclose(norm_f.min(), 0)
+        >>> assert isclose(norm_f.max(), 1)
+
+        >>> raw_u = (np.random.rand(8, 8) * 255).astype(np.uint8)
+        >>> norm_u = normalize(raw_u)
+
+        >>> raw_m = (np.zeros((8, 8)) + 10)
+        >>> norm_m = normalize(raw_m, min_val=0, max_val=20)
+        >>> assert isclose(norm_m.min(), 0.5)
+        >>> assert isclose(norm_m.max(), 0.5)
+
+    Example:
+        >>> # xdoctest: +REQUIRES(module:kwimage)
+        >>> import kwimage
+        >>> arr = kwimage.grab_test_image('lowcontrast')
+        >>> arr = kwimage.ensure_float01(arr)
+        >>> norms = {}
+        >>> norms['arr'] = arr.copy()
+        >>> norms['linear'] = normalize(arr, mode='linear')
+        >>> norms['sigmoid'] = normalize(arr, mode='sigmoid')
+        >>> # xdoctest: +REQUIRES(--show)
+        >>> import kwplot
+        >>> kwplot.autompl()
+        >>> kwplot.figure(fnum=1, doclf=True)
+        >>> pnum_ = kwplot.PlotNums(nSubplots=len(norms))
+        >>> for key, img in norms.items():
+        >>>     kwplot.imshow(img, pnum=pnum_(), title=key)
+
+    Benchmark:
+        # Our method is faster than standard in-line implementations.
+
+        import timerit
+        ti = timerit.Timerit(100, bestof=10, verbose=2, unit='ms')
+        arr = kwimage.grab_test_image('lowcontrast', dsize=(512, 512))
+
+        print('--- uint8 ---')
+        arr = ensure_float01(arr)
+        out = arr.copy()
+        for timer in ti.reset('naive1-float'):
+            with timer:
+                (arr - arr.min()) / (arr.max() - arr.min())
+
+        import timerit
+        for timer in ti.reset('simple-float'):
+            with timer:
+                max_ = arr.max()
+                min_ = arr.min()
+                result = (arr - min_) / (max_ - min_)
+
+        for timer in ti.reset('normalize-float'):
+            with timer:
+                normalize(arr)
+
+        for timer in ti.reset('normalize-float-inplace'):
+            with timer:
+                normalize(arr, out=out)
+
+        print('--- float ---')
+        arr = ensure_uint255(arr)
+        out = arr.copy()
+        for timer in ti.reset('naive1-uint8'):
+            with timer:
+                (arr - arr.min()) / (arr.max() - arr.min())
+
+        import timerit
+        for timer in ti.reset('simple-uint8'):
+            with timer:
+                max_ = arr.max()
+                min_ = arr.min()
+                result = (arr - min_) / (max_ - min_)
+
+        for timer in ti.reset('normalize-uint8'):
+            with timer:
+                normalize(arr)
+
+        for timer in ti.reset('normalize-uint8-inplace'):
+            with timer:
+                normalize(arr, out=out)
+
+    Ignore:
+        globals().update(xdev.get_func_kwargs(normalize))
+    """
+    if out is None:
+        out = arr.copy()
+
+    # TODO:
+    # - [ ] Parametarize new_min / new_max values
+    #     - [ ] infer from datatype
+    #     - [ ] explicitly given
+    new_min = 0.0
+    if arr.dtype.kind in ('i', 'u'):
+        # Need a floating point workspace
+        float_out = out.astype(np.float32)
+        new_max = float(np.iinfo(arr.dtype).max)
+    elif arr.dtype.kind == 'f':
+        float_out = out
+        new_max = 1.0
+    else:
+        raise NotImplementedError
+
+    # TODO:
+    # - [ ] Parametarize old_min / old_max strategies
+    #     - [X] explicitly given min and max
+    #     - [ ] raw-naive min and max inference
+    #     - [ ] outlier-aware min and max inference
+    old_min = float_out.min() if min_val is None else min_val
+    old_max = float_out.max() if max_val is None else max_val
+
+    old_span = old_max - old_min
+    new_span = new_max - new_min
+
+    if mode == 'linear':
+        # linear case
+        # out = (arr - old_min) * (new_span / old_span) + new_min
+        factor = 1.0 if old_span == 0 else (new_span / old_span)
+        if old_min != 0:
+            float_out -= old_min
+    elif mode == 'sigmoid':
+        # nonlinear case
+        # out = new_span * sigmoid((arr - beta) / alpha) + new_min
+        from scipy.special import expit as sigmoid
+        if beta is None:
+            # should center the desired distribution to visualize on zero
+            beta = old_max - old_min
+
+        if alpha is None:
+            # division factor
+            # from scipy.special import logit
+            # alpha = max(abs(old_min - beta), abs(old_max - beta)) / logit(0.998)
+            # This chooses alpha such the original min/max value will be pushed
+            # towards -1 / +1.
+            alpha = max(abs(old_min - beta), abs(old_max - beta)) / 6.212606
+
+        if isclose(alpha, 0):
+            alpha = 1
+
+        energy = float_out
+        energy -= beta
+        energy /= alpha
+        # Ideally the data of interest is roughly in the range (-6, +6)
+        float_out = sigmoid(energy, out=float_out)
+        factor = new_span
+    else:
+        raise KeyError(mode)
+
+    # Stretch / shift to the desired output range
+    if factor != 1:
+        float_out *= factor
+
+    if new_min != 0:
+        float_out += new_min
+
+    if float_out is not out:
+        out[:] = float_out.astype(out.dtype)
+
+    return out