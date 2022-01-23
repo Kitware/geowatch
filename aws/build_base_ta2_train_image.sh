ca5ebdb2be85ce36362f453b3c5a71c0a6739c98
joncrall <jon.crall@kitware.com>
2022-01-22 20:24:12 -0500

    typos

--- aws/build_base_ta2_train_image.sh
+++ aws/build_base_ta2_train_image.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 __doc__='
 load_secrets
-docker login gitlab.kitware.com:4567 --username $AD_USERNAME --password $AD_PASSWORD
-docker pull gitlab.kitware.com:4567/computer-vision/ci-docker/miniconda3
+docker login gitlab.kitware.com:4567 --username $GITLAB_KITWARE_USERNAME --password $GITLAB_KITWARE_TOKEN
 '
 #docker login gitlab.kitware.com:4567
 
 cd "$HOME/code/watch/"
 docker build -t "smart/watch/ta2_training_v2" -f ./dockerfiles/ta2_training_v2.Dockerfile --build-arg BUILD_STRICT=1 .
 
-docker tag smart/watch/ta2-train-v2 gitlab.kitware.com:4567/smart/watch/ta2_training_v2
+docker tag smart/watch/ta2_training_v2 gitlab.kitware.com:4567/smart/watch/ta2_training_v2
 docker push gitlab.kitware.com:4567/smart/watch/ta2_training_v2
 
 # f887e42a2206
