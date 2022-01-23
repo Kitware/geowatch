a7e9f5cb5a39b7a9e2f08684f2c47312c34bf458
joncrall <jon.crall@kitware.com>
2022-01-22 22:59:43 -0500

    do I have a working argo submit?

--- aws/submit_argo_jobs.sh
+++ aws/submit_argo_jobs.sh
@@ -1,11 +1,8 @@
 # TODO: need to change name to indicate this is training.
 
-
 cd "$HOME/code/watch/aws"
 #argo submit ta2_train_workflow.yml --watch
 argo submit dvc_check_workflow.yml --watch
-argo submit dvc_access_check.yml --watch
-
 
 
 watch_recent_argo_logs(){
@@ -14,7 +11,6 @@ watch_recent_argo_logs(){
     echo "WORKFLOW_NAME = $WORKFLOW_NAME"
     argo logs "${WORKFLOW_NAME}" --follow
 
-
-    argo delete
-
+    #argo delete "dvc-access-check-*"
+    #argo delete dvc-access-check-bswmx   dvc-access-check-wd7qk   dvc-access-check-zcggj   dvc-access-check-vhc9k   dvc-access-check-gtdlr   dvc-access-check-mqttg   dvc-access-check-g6vpc   dvc-access-check-hwvzf   dvc-access-check-kmd4k   
 }
