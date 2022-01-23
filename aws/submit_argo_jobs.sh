1cfe6eeceadfbc02b6f1cf2e05cb5233966f6b28
joncrall <jon.crall@kitware.com>
2022-01-22 22:44:21 -0500

    wip

--- aws/submit_argo_jobs.sh
+++ aws/submit_argo_jobs.sh
@@ -1,5 +1,15 @@
-
 # TODO: need to change name to indicate this is training.
-argo submit dvc_check_workflow.yml --watch
 
 
+
+cd "$HOME/code/watch/aws"
+argo submit ta2_train_workflow.yml --watch
+
+
+
+watch_recent_argo_logs(){
+    # This is not 100% reliable has race conditions
+    WORKFLOW_NAME=$(argo list --running | head -n 2 | tail -n 1 | cut -d' ' -f1)
+    echo "WORKFLOW_NAME = $WORKFLOW_NAME"
+    argo logs "${WORKFLOW_NAME}" --follow
+}
