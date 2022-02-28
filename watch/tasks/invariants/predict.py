7c79b18d33552d524f1984e58719dae2825050f9
benjaminbrodie <benjaminbrodie21@gmail.com>
2022-02-28 17:06:07 -0500

    correction to normalization

--- watch/tasks/invariants/predict.py
+++ watch/tasks/invariants/predict.py
@@ -148,7 +148,7 @@ class predict(object):
                     if args.do_pca:
                         features = torch.einsum('xy,byhw->bxhw', self.pca_projector, features)
                     ###normalize features
-                    features = (features - features.mean(dim=(3, 4))) / features.std(dim=(3, 4))
+                    features = (features - features.mean(dim=(2, 3))) / features.std(dim=(2, 3))
                     save_feat.append(torch.sigmoid(features.squeeze()).permute(1, 2, 0).cpu())
 
                 if 'before_after' in args.tasks:
