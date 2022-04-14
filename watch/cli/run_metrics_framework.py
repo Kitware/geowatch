5693f7389ebc6a7a352e29e51ab667503b49508e
joncrall <jon.crall@kitware.com>
2022-04-14 16:27:50 -0400

    wip

--- watch/cli/run_metrics_framework.py
+++ watch/cli/run_metrics_framework.py
@@ -821,7 +821,7 @@ def main(args):
         ]
         run_eval_command += viz_flags
         # run metrics framework
-        cmd = '{virtualenv_cmd} &&' + ' '.join(list(map(str, run_eval_command)))
+        cmd = f'{virtualenv_cmd} &&' + ' '.join(list(map(str, run_eval_command)))
         # ub.codeblock(fr'''
         #     {virtualenv_cmd} &&
         #     python -m iarpa_smart_metrics.run_evaluation \
