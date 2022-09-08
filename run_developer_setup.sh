105c81b793a7fae9ecd30af0125b9a2735f0a193
joncrall <jon.crall@kitware.com>
2022-09-08 12:38:41 -0400

    wip

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -12,6 +12,62 @@ if [[ "$VIRTUAL_ENV" == "" ]]; then
     echo "NOT INSIDE OF A VIRTUALENV. This script may not run correctly"
 fi 
 
+apt_ensure(){
+    __doc__="
+    Checks to see if the packages are installed and installs them if needed.
+
+    The main reason to use this over normal apt install is that it avoids sudo
+    if we already have all requested packages.
+
+    Args:
+        *ARGS : one or more requested packages 
+
+    Example:
+        apt_ensure git curl htop 
+
+    Ignore:
+        REQUESTED_PKGS=(git curl htop) 
+    "
+    # Note the $@ is not actually an array, but we can convert it to one
+    # https://linuxize.com/post/bash-functions/#passing-arguments-to-bash-functions
+    ARGS=("$@")
+    MISS_PKGS=()
+    HIT_PKGS=()
+    for PKG_NAME in "${ARGS[@]}"
+    do
+        #apt_ensure_single $EXE_NAME
+        RESULT=$(dpkg -l "$PKG_NAME" | grep "^ii *$PKG_NAME")
+        if [ "$RESULT" == "" ]; then 
+            echo "Do not have PKG_NAME='$PKG_NAME'"
+            # shellcheck disable=SC2268,SC2206
+            MISS_PKGS=(${MISS_PKGS[@]} "$PKG_NAME")
+        else
+            echo "Already have PKG_NAME='$PKG_NAME'"
+            # shellcheck disable=SC2268,SC2206
+            HIT_PKGS=(${HIT_PKGS[@]} "$PKG_NAME")
+        fi
+    done
+
+    if [ "${#MISS_PKGS}" -gt 0 ]; then
+        sudo apt install -y "${MISS_PKGS[@]}"
+    else
+        echo "No missing packages"
+    fi
+}
+
+###  ENSURE DEPENDENCIES ###
+
+# If on debian/ubuntu ensure the dependencies are installed 
+if [[ "$(command -v apt)" != "" ]]; then
+    apt_ensure ffmpeg tmux jq tree p7zip-full rsync
+else
+    echo "
+    WARNING: Check and install of system packages is currently only supported
+    on Debian Linux. You will need to verify that ZLIB, GSL, OpenMP are
+    installed before running this script.
+    "
+fi
+
 #pip install -r requirements/no-deps.txt
 
 # Do everything
@@ -19,25 +75,22 @@ python -m pip install pip setuptools wheel build -U
 
 # FIXME: Something in the req is pinning scikit-image to be less than 0.19 but
 # I'm not sure what it is.
-pip install scikit-image
+#pip install scikit-image
 
 python -m pip install -r requirements.txt -v
 
-# Install the watch module in development mode
-python -m pip install -e .
+python -m pip install -r requirements/gdal.txt
 
-# Install more fragile dependencies
-# pip install imgaug>=0.4.0
-# pip install netharn>=0.5.16
-#pip install GDAL>=3.5.0 --find-links https://girder.github.io/large_image_wheels -U
+python -m pip install -r requirements/headless.txt
 
-# TODO: we should skip trying to install gdal if possible
-
-python -m pip install GDAL==3.4.1 --find-links https://girder.github.io/large_image_wheels -U
+python -m pip install -r requirements/linting.txt
 
 python -m pip install dvc[all]>=2.9.3
 
 python -m pip install lru-dict || echo "unable to install lru-dict"
+
+# Install the watch module in development mode
+python -m pip install -e .
 
 fix_opencv_conflicts(){
     __doc__="
