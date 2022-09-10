1d82fbc7d1ce8e152094b8f39e75793d45dcbf79
Connor Greenwell <connor.greenwell@arthur.khq.kitware.com>
2022-09-10 10:53:54 -0400

    adds binary torch metrics, these can be removed after the cited PR is released around torchmetrics v0.10

---
+++ watch/utils/util_torchmetrics.py
@@ -0,0 +1,39 @@
+'''
+This is a stopgap measure until https://github.com/Lightning-AI/metrics/pull/1195 is integrated
+integrated into torchmetrics (targeted for v0.10?). The main issue that these metrics solve is
+that when computing classification stats for *binary* problems, the returned score is an average
+over the two classes stats when most of the time we only want one of them. This is especially
+necessary in imbalanced settings where the metric for the largest class completely obscures the
+impact of the smaller class.
+'''
+
+import torchmetrics
+
+class BinaryF1Score(torchmetrics.StatScores):
+    def compute(self):
+        tp, fp, tn, fn = self._get_final_stats()
+        tp, fp, tn, fn = tp[1], fp[1], tn[1], fn[1]
+        return (2*tp) / ((2*tp) + fp + fn)
+    
+class BinaryOverallAccuracy(torchmetrics.StatScores):
+    def compute(self):
+        tp, fp, tn, fn = self._get_final_stats()
+        tp, fp, tn, fn = tp[1], fp[1], tn[1], fn[1]
+        return (tp + tn) / (tp + fp + tn + fn)
+    
+class BinaryBalancedAccuracy(torchmetrics.StatScores):
+    def compute(self):
+        tp, fp, tn, fn = self._get_final_stats()
+        tp, fp, tn, fn = tp[1], fp[1], tn[1], fn[1]
+        
+        tpr = tp / (tp + fn)
+        tnr = tn / (tn + fp)
+        
+        return (tpr + tnr) / 2
+    
+class BinaryTruePositiveRate(torchmetrics.StatScores):
+    def compute(self):
+        tp, fp, tn, fn = self._get_final_stats()
+        tp, fp, tn, fn = tp[1], fp[1], tn[1], fn[1]
+        
+        return tp / (tp + fn)
