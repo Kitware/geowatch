08d59515464664ad8721988ca20bda709986506b
Connor Greenwell <connor.greenwell@arthur.khq.kitware.com>
2022-10-12 09:10:29 -0400

    lint

--- watch/utils/util_torchmetrics.py
+++ watch/utils/util_torchmetrics.py
@@ -1,3 +1,12 @@
+import torchmetrics
+
+import warnings
+warnings.warn('''
+    util_torchmetrics is deprecated since torchmetrics==0.10.0.
+    Consider replacing calls to `util_torchmetrics.Binary{Metric}(...)`
+    with calls to `torchmetrics.{Metric}(..., task='binary')`.
+''', DeprecationWarning, stacklevel=2)
+
 '''
 This is a stopgap measure until https://github.com/Lightning-AI/metrics/pull/1195 is integrated
 integrated into torchmetrics (targeted for v0.10?). The main issue that these metrics solve is
@@ -6,15 +15,6 @@ over the two classes stats when most of the time we only want one of them. This 
 necessary in imbalanced settings where the metric for the largest class completely obscures the
 impact of the smaller class.
 '''
-import warnings
-warnings.warn('''
-    util_torchmetrics is deprecated since torchmetrics==0.10.0. 
-    Consider replacing calls to `util_torchmetrics.Binary{Metric}(...)`
-    with calls to `torchmetrics.{Metric}(..., task='binary')`.
-''', DeprecationWarning, stacklevel=2)
-
-
-import torchmetrics
 
 
 class BinaryF1Score(torchmetrics.StatScores):
