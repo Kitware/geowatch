39dc5ac51351040f1a2f7332e5f351eb4bcfc921
Connor Greenwell <connor.greenwell@arthur.khq.kitware.com>
2022-10-24 10:57:06 -0400

    HeterogeneousModel superceeds SequenceAwareModel, deleting

--- watch/tasks/fusion/methods/__init__.py
+++ watch/tasks/fusion/methods/__init__.py
@@ -4,9 +4,6 @@ mkinit -m watch.tasks.fusion.methods -w
 from watch.tasks.fusion.methods import channelwise_transformer
 from watch.tasks.fusion.methods.channelwise_transformer import (
     MultimodalTransformer)
-from watch.tasks.fusion.methods import sequence_aware
-from watch.tasks.fusion.methods.sequence_aware import (
-    SequenceAwareModel)
 from watch.tasks.fusion.methods import heterogeneous
 from watch.tasks.fusion.methods.heterogeneous import (
     HeterogeneousModel)
@@ -14,4 +11,4 @@ from watch.tasks.fusion.methods import noop_model
 from watch.tasks.fusion.methods.noop_model import (
     NoopModel)
 
-__all__ = ['MultimodalTransformer', 'channelwise_transformer', 'SequenceAwareModel', 'sequence_aware', 'heterogeneous', 'HeterogeneousModel', 'noop_model', 'NoopModel']
+__all__ = ['MultimodalTransformer', 'channelwise_transformer', 'heterogeneous', 'HeterogeneousModel', 'noop_model', 'NoopModel']
