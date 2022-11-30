7d8e287744884093a2247723e552fd0b42d53ce7
joncrall <jon.crall@kitware.com>
2022-11-30 16:52:48 -0500

    Drop6 script

--- scripts/prepare_drop5.sh
+++ scripts/prepare_drop5.sh
@@ -3,7 +3,7 @@
 source "$HOME"/code/watch/secrets/secrets
 
 DATA_DVC_DPATH=$(smartwatch_dvc --tags=phase2_data --hardware="auto")
-SENSORS=TA1-S2-L8-WV-PD-ACC-2
+SENSORS=TA1-S2-L8-WV-PD-ACC-1
 DATASET_SUFFIX=Drop5-2022-11-07-c30-$SENSORS
 REGION_GLOBSTR="$DATA_DVC_DPATH/annotations/region_models/*.geojson"
 SITE_GLOBSTR="$DATA_DVC_DPATH/annotations/site_models/*.geojson"
@@ -67,38 +67,13 @@ print('total = {}'.format(xd.byte_str(total_size)))
 
 "
 
-
-
-
-#DATA_DVC_DPATH=$(smartwatch_dvc --tags=phase2_data --hardware="hdd")
-#SENSORS=TA1-S2-L8-WV-PD-ACC
-#DATASET_SUFFIX=TestRegionKW4
-#REGION_GLOBSTR="/home/joncrall/Downloads/KW_test4.geojson"
-#SITE_GLOBSTR=""
-
-#export GDAL_DISABLE_READDIR_ON_OPEN=EMPTY_DIR
-
-## Construct the TA2-ready dataset
-#python -m watch.cli.prepare_ta2_dataset \
-#    --dataset_suffix=$DATASET_SUFFIX \
-#    --stac_query_mode=auto \
-#    --cloud_cover=30 \
-#    --sensors="$SENSORS" \
-#    --api_key=env:SMART_STAC_API_KEY \
-#    --collated True \
-#    --dvc_dpath="$DATA_DVC_DPATH" \
-#    --aws_profile=iarpa \
-#    --region_globstr="$REGION_GLOBSTR" \
-#    --site_globstr="$SITE_GLOBSTR" \
-#    --requester_pays=False \
-#    --fields_workers=20 \
-#    --convert_workers=8 \
-#    --max_queue_size=2 \
-#    --align_workers=12 \
-#    --cache=1 \
-#    --ignore_duplicates=1 \
-#    --separate_region_queues=1 \
-#    --separate_align_jobs=1 \
-#    --visualize=0 \
-#    --target_gsd=10 \
-#    --backend=tmux --run=1
+add_dvc_data(){
+    DATA_DVC_DPATH=$(smartwatch_dvc --tags='phase2_data' --hardware=hdd)
+    cd "$DATA_DVC_DPATH"
+    ln -s "Aligned-Drop5-2022-11-07-c30-TA1-S2-L8-WV-PD-ACC-1" "Drop5"
+    git add Drop5
+    cd "$DATA_DVC_DPATH/Drop5"
+    python -m watch.cli.prepare_splits data.kwcoco.json --cache=0 --run=1
+    7z a splits.zip data*.kwcoco.json imganns-*.kwcoco.json
+    dvc add -- */L8 */S2 */WV *.zip && dvc push -r horologic -R . && git commit -am "Add Drop5 ACC-1" && git push 
+}
