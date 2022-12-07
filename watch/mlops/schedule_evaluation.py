19e1280d158e529c2b36498d46c399ff1b1a15b8
joncrall <jon.crall@kitware.com>
2022-12-07 15:18:00 -0500

    test update

--- watch/mlops/schedule_evaluation.py
+++ watch/mlops/schedule_evaluation.py
@@ -76,17 +76,17 @@ Example:
                     - $DVC_EXPT_DPATH/package_epoch10_step200000.pt
                 bas_pxl.test_dataset:
                     - $DVC_DATA_DPATH/Drop4-BAS/KR_R001.kwcoco.json
-                    - $DVC_DATA_DPATH/Drop4-BAS/KR_R002.kwcoco.json
+                    # - $DVC_DATA_DPATH/Drop4-BAS/KR_R002.kwcoco.json
                 bas_pxl.window_space_scale:
                     - auto
                     # - "15GSD"
                     # - "30GSD"
-                bas_pxl.chip_dims:
-                    - "256,256"
+                # bas_pxl.chip_dims:
+                #     - "256,256"
                 bas_pxl.time_sampling:
                     - "auto"
-                bas_pxl.input_space_scale:
-                    - "window"
+                # bas_pxl.input_space_scale:
+                #     - "window"
                 bas_poly.moving_window_size:
                     - null
                     # - 100
@@ -107,19 +107,19 @@ Example:
                     - 0
                 sc_pxl.package_fpath:
                     - $DVC_EXPT_DPATH/models/fusion/Drop4-SC/packages/Drop4_tune_V30_8GSD_V3/Drop4_tune_V30_8GSD_V3_epoch=2-step=17334.pt.pt
-                bas_poly_eval.enabled: 0
-                bas_pxl_eval.enabled: 0
-                bas_poly_viz.enabled: 0
-                sc_poly_eval.enabled: 0
-                sc_pxl_eval.enabled: 0
+                bas_poly_eval.enabled: 1
+                bas_pxl_eval.enabled: 1
+                bas_poly_viz.enabled: 1
+                sc_poly_eval.enabled: 1
+                sc_pxl_eval.enabled: 1
                 sc_poly_viz.enabled: 1
         " \
         --root_dpath=$DVC_EXPT_DPATH/_testpipe \
         --enable_links=1 \
         --devices="0,1" --queue_size=2 \
-        --backend=tmux \
+        --backend=serial \
         --pipeline=bas \
-        --cache=1 --rprint=1 --run=0
+        --cache=1 --rprint=1 --run=1
 
         --pipeline=joint_bas_sc
 
