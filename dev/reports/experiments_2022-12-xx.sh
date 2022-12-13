27e924d830089dd8f075e94b74addd19f949b206
joncrall <jon.crall@kitware.com>
2022-12-13 10:48:04 -0500

    wip

--- dev/reports/experiments_2022-12-xx.sh
+++ dev/reports/experiments_2022-12-xx.sh
@@ -43,6 +43,8 @@ python -m watch.mlops.schedule_evaluation \
         matrix:
             bas_pxl.package_fpath:
                 - $DVC_EXPT_DPATH/models/fusion/Drop4-BAS/packages/Drop4_TuneV323_BAS_30GSD_BGRNSH_V2/package_epoch0_step41.pt.pt
+                - /home/joncrall/remote/toothbrush/data/dvc-repos/smart_expt_dvc/bas_upsampled_epoch28.pt
+                - /home/joncrall/remote/toothbrush/data/dvc-repos/smart_expt_dvc/bas_native_epoch44.pt
             bas_pxl.test_dataset:
                 - $DVC_DATA_DPATH/Drop4-BAS/KR_R001.kwcoco.json
                 - $DVC_DATA_DPATH/Drop4-BAS/KR_R002.kwcoco.json
@@ -56,6 +58,7 @@ python -m watch.mlops.schedule_evaluation \
                 - 15GSD
                 - 30GSD
             bas_pxl.chip_dims:
+                - auto
                 - 256,256
             bas_pxl.time_sampling:
                 - auto
@@ -65,11 +68,11 @@ python -m watch.mlops.schedule_evaluation \
                 - null
                 - 100
                 - 200
+                - 300
             bas_poly.thresh:
                 - 0.1
                 - 0.13
                 - 0.2
-
             sc_pxl.chip_dims:
                 - auto
                 - 256,256
