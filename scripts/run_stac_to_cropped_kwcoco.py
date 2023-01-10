e28f84c09c97aa078667cc1f02b19b2a08e65702
David Joy <david.joy@kitware.com>
2023-01-10 11:42:15 -0500

    Add cleanup scripts for BAS and SC dataset generation

--- scripts/run_stac_to_cropped_kwcoco.py
+++ scripts/run_stac_to_cropped_kwcoco.py
@@ -285,7 +285,18 @@ def run_stac_to_cropped_kwcoco(input_path,
                     '--aux_workers', str(include_channels.count('|') + 1),
                     '--rpc_align_method', 'affine_warp'], check=True)
 
-    # 5. Egress (envelop KWCOCO dataset in a STAC item and egress;
+    # 5. "Clean" dataset
+    # TODO: Ask Jon about channels (can it include 'quality'?)
+    subprocess.run(['python', '-m', 'watch.cli.coco_clean_geotiffs',
+                    '--src', ta1_cropped_kwcoco_path,
+                    '--channels', "red|green|blue|nir|swir16|swir22",
+                    '--prefilter_channels', "red",
+                    '--min_region_size', '256',
+                    '--nodata_value', '-9999',
+                    '--workers', '1' if force_one_job_for_cropping else str(jobs),  # noqa: 501
+                    ], check=True)
+
+    # 6. Egress (envelop KWCOCO dataset in a STAC item and egress;
     #    will need to recursive copy the kwcoco output directory up to
     #    S3 bucket)
     print("* Egressing KWCOCO dataset and associated STAC item *")
