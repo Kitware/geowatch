cc9f66fff3b278c06fe0103e1dce2190310f44cf
Connor Greenwell <connor.greenwell@horologic.khq.kitware.com>
2023-01-19 14:09:52 -0500

    skips empty sites during tracker run. plays better with mlops framework this way

--- watch/tasks/tracking/utils.py
+++ watch/tasks/tracking/utils.py
@@ -65,14 +65,17 @@ class TrackFunction(collections.abc.Callable):
                                 total=total,
                                 desc='apply_per_video',
                                 verbose=3):
-            sub_dset = self.safe_apply(coco_dset,
-                                       gids,
-                                       overwrite,
-                                       legacy=legacy)
-            if legacy:
-                coco_dset = sub_dset
-            else:
-                tracked_subdsets.append(sub_dset)
+            try:
+                sub_dset = self.safe_apply(coco_dset,
+                                           gids,
+                                           overwrite,
+                                           legacy=legacy)
+                if legacy:
+                    coco_dset = sub_dset
+                else:
+                    tracked_subdsets.append(sub_dset)
+            except ValueError as e:
+                print(e)
 
         if not legacy:
             # Tracks were either updated or added.
