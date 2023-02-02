5193bc361ba28991ec834ccaf0ef0518ddf318a3
joncrall <jon.crall@kitware.com>
2023-02-02 18:40:35 -0500

    Bump version

--- watch/__init__.py
+++ watch/__init__.py
@@ -6,7 +6,7 @@ import ubelt as ub
 import warnings
 
 
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 # git shortlog -e --summary --numbered
 __author__ = 'WATCH Developers, Kitware Inc., Jon Crall, David Joy, Matthew Bernstein, Benjamin Brodie, Usman Rafique, Jacob DeRosa, Connor Greenwell, Peri Akiva, Matthew Purri, Ajay Upadhyaya'
 __author_email__ = 'kitware@kitware.com, jon.crall@kitware.com'
