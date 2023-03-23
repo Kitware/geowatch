d89de9981e6442e9e4986aa0c70d5b8321356a9a
joncrall <jon.crall@kitware.com>
2023-03-23 14:35:25 -0400

    Add CLI tests

---
+++ tests/test_cli.py
@@ -0,0 +1,41 @@
+
+
+def test_cli_help_message():
+    verbose = 0
+
+    import ubelt as ub
+    info = ub.cmd('python -m watch.cli --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli align --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli dvcdir --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli find_dvc --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli stats --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli add_fields --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli model_stats --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli reproject --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli average --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli iarpa_eval --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli spectra --help', verbose=verbose)
+    assert info['ret'] == 0
+
+    info = ub.cmd('python -m watch.cli visualize --help', verbose=verbose)
+    assert info['ret'] == 0
