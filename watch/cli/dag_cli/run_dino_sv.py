7c80d4f1399c96f4e40da01bdd36c977731f8bf0
David Joy <david.joy@kitware.com>
2023-04-28 13:58:10 -0400

    Add option to collate output of dino_sv

--- watch/cli/dag_cli/run_dino_sv.py
+++ watch/cli/dag_cli/run_dino_sv.py
@@ -42,6 +42,11 @@ class DinoSVConfig(scfg.DataConfig):
             '''
             S3 Output directory for STAC item / asset egress
             '''))
+    ta2_s3_collation_bucket = scfg.Value(None, type=str, help=ub.paragraph(
+            '''
+            S3 Location for collated TA-2 output (bucket name should
+            include up to eval name)
+            '''))
     dino_detect_config = scfg.Value(None, type=str, help=ub.paragraph(
             '''
             Raw json/yaml or a path to a json/yaml file that specifies the
@@ -87,6 +92,32 @@ def _upload_region(aws_base_command,
               "not uploading")
 
 
+def _ta2_collate_output(aws_base_command,
+                        local_region_dir,
+                        local_sites_dir,
+                        destination_s3_bucket,
+                        performer_suffix='KIT'):
+    def _get_suffixed_basename(local_path):
+        base, ext = os.path.splitext(os.path.basename(local_path))
+        return "{}_{}{}".format(base, performer_suffix, ext)
+
+    for region in glob(os.path.join(local_region_dir, '*.geojson')):
+        region_s3_outpath = '/'.join((destination_s3_bucket,
+                                      'region_models',
+                                      _get_suffixed_basename(region)))
+        subprocess.run([*aws_base_command,
+                        region,
+                        region_s3_outpath], check=True)
+
+    for site in glob(os.path.join(local_sites_dir, '*.geojson')):
+        site_s3_outpath = '/'.join((destination_s3_bucket,
+                                    'site_models',
+                                    _get_suffixed_basename(site)))
+        subprocess.run([*aws_base_command,
+                        site,
+                        site_s3_outpath], check=True)
+
+
 def run_dino_sv(config):
     from watch.cli.baseline_framework_kwcoco_ingress import baseline_framework_kwcoco_ingress
     from watch.cli.baseline_framework_kwcoco_egress import baseline_framework_kwcoco_egress
@@ -98,6 +129,7 @@ def run_dino_sv(config):
     output_path = config.output_path
 
     outbucket = config.outbucket
+    ta2_s3_collation_bucket = config.ta2_s3_collation_bucket
     aws_profile = config.aws_profile
     dryrun = config.dryrun
 
@@ -210,7 +242,15 @@ def run_dino_sv(config):
 
         ub.cmd(dino_building_filter.command(), check=True, verbose=3, system=True)
 
-    # 4. Egress (envelop KWCOCO dataset in a STAC item and egress;
+    # 4. (Optional) collate TA-2 output
+    if ta2_s3_collation_bucket is not None:
+        print("* Collating TA-2 output")
+        _ta2_collate_output(aws_base_command,
+                            region_models_outdir,
+                            site_models_outdir,
+                            ta2_s3_collation_bucket)
+
+    # 5. Egress (envelop KWCOCO dataset in a STAC item and egress;
     #    will need to recursive copy the kwcoco output directory up to
     #    S3 bucket)
     print("* Egressing KWCOCO dataset and associated STAC item *")
