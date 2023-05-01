f3c754e7c02265533b92c758111924f8caf788c0
joncrall <jon.crall@kitware.com>
2023-05-01 14:40:12 -0400

    tweak

--- dev/devsetup/make_quicklinks.sh
+++ dev/devsetup/make_quicklinks.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir -p  "$HOME"/quicklinks/
 
-ln -sf  "$HOME"/remote/namek/data/dvc-repos/smart_expt_dvc "$HOME"/quicklinks/namek_smart_data_dvc
+ln -sf  "$HOME"/remote/namek/data/dvc-repos/smart_expt_dvc "$HOME"/quicklinks/smart_expt_dvc_dvc
 
 ln -sf  "$HOME"/remote/toothbrush/data/dvc-repos/smart_data_dvc-hdd "$HOME"/quicklinks/toothbrush_smart_data_dvc-hdd
 ln -sf  "$HOME"/remote/toothbrush/data/dvc-repos/smart_data_dvc-ssd "$HOME"/quicklinks/toothbrush_smart_data_dvc-ssd
 ln -sf  "$HOME"/remote/toothbrush/data/dvc-repos/smart_expt_dvc "$HOME"/quicklinks/toothbrush_smart_expt_dvc
+
+
+ln -sf  "$HOME"/remote/ooo/data/dvc-repos/smart_data_dvc-ssd "$HOME"/quicklinks/ooo_smart_data_dvc-ssd
