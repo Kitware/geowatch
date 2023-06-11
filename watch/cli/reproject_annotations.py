924023283eb5b32b07de30f1b37ca49195f26827
joncrall <jon.crall@kitware.com>
2023-06-10 21:39:18 -0400

    Fix tensorflow requirement

--- watch/cli/reproject_annotations.py
+++ watch/cli/reproject_annotations.py
@@ -501,6 +501,12 @@ def expand_site_models_with_site_summaries(sites, regions, validate_checks=True)
 
     if site_rows2:
         site_df2 = pd.concat(site_rows2).reset_index()
+        if 'site_id' not in site_df2.columns:
+            print(f'{len(site_df2)=}')
+            print(f'{site_df2.columns=}')
+            print('site_df2:')
+            print(site_df2)
+            raise Exception('site_id column is not in site_df2. See previous output for details')
         if len(set(site_df2['site_id'])) != len(site_df2):
             counts = site_df2['site_id'].value_counts()
             duplicates = counts[counts > 1]
