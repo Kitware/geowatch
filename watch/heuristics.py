4e0c8bdb41fe58eb2e23b4a9179e000dc72a0f9f
Connor Greenwell <connor.greenwell@horologic.khq.kitware.com>
2023-08-09 10:27:44 -0400

    cherry pick most needed bits from transient prep MR

--- watch/heuristics.py
+++ watch/heuristics.py
@@ -399,6 +399,13 @@ CATEGORIES = [
         'scored': True,
         'tags': ['positive'],
     },
+    # Transient
+    {
+        'name': 'transient',
+        'color': 'steelblue',
+        'scored': True,
+        'tags': ['positive'],
+    },
 ]
 
 
