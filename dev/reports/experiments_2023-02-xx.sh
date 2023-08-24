73d13419f1b3e09254de3e81cdb2c0a22ca57b9b
joncrall <jon.crall@kitware.com>
2023-08-23 23:42:43 -0400

    wip

--- dev/reports/experiments_2023-02-xx.sh
+++ dev/reports/experiments_2023-02-xx.sh
@@ -3260,30 +3260,89 @@ geowatch schedule --params="
             - $DVC_EXPT_DPATH/models/depth_pcd/basicModel2.h5
         sv_depth_filter.threshold:
             - 0.10
+
+        #####################
+        ## SC PIXEL PARAMS ##
+        #####################
+
+        sc_pxl.package_fpath:
+            - $DVC_EXPT_DPATH/models/fusion/Drop4-SC/packages/Drop4_tune_V30_8GSD_V3/Drop4_tune_V30_8GSD_V3_epoch=2-step=17334.pt.pt
+        sc_pxl.tta_fliprot: 0.0
+        sc_pxl.tta_time: 0.0
+        sc_pxl.chip_overlap: 0.3
+        sc_pxl.input_space_scale: 2GSD
+        sc_pxl.window_space_scale: 2GSD
+        sc_pxl.output_space_scale: 2GSD
+        #sc_pxl.time_span: 6m
+        #sc_pxl.time_sampling: auto
+        #sc_pxl.time_steps: 12
+        #sc_pxl.chip_dims: auto
+        sc_pxl.set_cover_algo: null
+        sc_pxl.resample_invalid_frames: 3
+        sc_pxl.observable_threshold: 0.0
+        sc_pxl.mask_low_quality: false
+        sc_pxl.drop_unused_frames: true
+        #sc_pxl.num_workers: 12
+        #sc_pxl.batch_size: 1
+        sc_pxl.write_workers: 0
+
+        #####################
+        ## SC POLY PARAMS  ##
+        #####################
+
+        sc_poly.thresh: 0.07
+        sc_poly.boundaries_as: polys
+        sc_poly.resolution: 2GSD
+        sc_poly.min_area_square_meters: 7200
+
+        ##########################
+        ## SC POLY EVAL PARAMS  ##
+        ##########################
+
+        sc_poly_eval.true_site_dpath: $DVC_DATA_DPATH/annotations/drop7/site_models
+        sc_poly_eval.true_region_dpath: $DVC_DATA_DPATH/annotations/drop7/region_models
+
+        ##################################
+        ## HIGH LEVEL PIPELINE CONTROLS ##
+        ##################################
+        sc_pxl.enabled: 1
+        sc_pxl_eval.enabled: 1
+        sc_poly.enabled: 1
+        sc_poly_eval.enabled: 1
+        sc_poly_viz.enabled: 0
     submatrices:
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/KR_R001/imganns-KR_R001.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/KR_R001/imgonly-KR_R001.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/KR_R001/imgonly-KR_R001.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/KR_R002/imganns-KR_R002.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/KR_R002/imgonly-KR_R002.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/KR_R002/imgonly-KR_R002.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/AE_R001/imganns-AE_R001.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/AE_R001/imgonly-AE_R001.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/AE_R001/imgonly-AE_R001.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/BR_R002/imganns-BR_R002.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/BR_R002/imgonly-BR_R002.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/BR_R002/imgonly-BR_R002.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/CH_R001/imganns-CH_R001.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/CH_R001/imgonly-CH_R001.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/CH_R001/imgonly-CH_R001.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/NZ_R001/imganns-NZ_R001.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/NZ_R001/imgonly-NZ_R001.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/NZ_R001/imgonly-NZ_R001.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/PE_R001/imganns-PE_R001.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/PE_R001/imgonly-PE_R001.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/PE_R001/imgonly-PE_R001.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/BR_R004/imganns-BR_R004.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/BR_R004/imgonly-BR_R004.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/BR_R004/imgonly-BR_R004.kwcoco.zip
         - bas_pxl.test_dataset: $DVC_DATA_DPATH/Drop7-MedianNoWinter10GSD-NoMask/CN_C000/imganns-CN_C000.kwcoco.zip
           sv_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/CN_C000/imgonly-CN_C000.kwcoco.zip
+          sc_crop.crop_src_fpath: $DVC_HDD_DATA_DPATH/Aligned-Drop7/CN_C000/imgonly-CN_C000.kwcoco.zip
     " \
     --root_dpath="$DVC_EXPT_DPATH/_namek_eval15_bas_nomask" \
-    --devices="0,1" --tmux_workers=4 \
+    --devices="0,1" --tmux_workers=8 \
     --backend=tmux --queue_name "_namek_eval15_bas_nomask" \
-    --pipeline=bas_building_and_depth_vali \
+    --pipeline=joint_bas_sv_sc \
     --skip_existing=1 \
     --run=1
 
@@ -3383,9 +3442,10 @@ geowatch schedule --params="
     --root_dpath="$DVC_EXPT_DPATH/_namek_eval15_bas_yesmask" \
     --devices="0,1" --tmux_workers=4 \
     --backend=tmux --queue_name "_namek_eval15_bas_yesmask" \
-    --pipeline=bas_building_and_depth_vali \
+    --pipeline=joint_bas_sv_sc \
     --skip_existing=1 \
     --run=1
+    #--pipeline=bas_building_and_depth_vali \
 
 
 # NO MASK
@@ -3422,7 +3482,7 @@ python -m watch.mlops.aggregate \
         show_csv: 0
     " \
     --rois="KR_R002,NZ_R001,CH_R001,KR_R001,CN_C000"
-    --rois="KR_R002,CN_C000"
+    #--rois="KR_R002,CN_C000"
 
 # VS MASKED
 DVC_EXPT_DPATH=$(geowatch_dvc --tags='phase2_expt' --hardware=auto)
