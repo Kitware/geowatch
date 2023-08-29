1ddbc2820a49dd37a68caccdf068fdb258f74e25
joncrall <jon.crall@kitware.com>
2023-08-29 11:01:37 -0400

    Add dev files

---
+++ dev/oneoffs/make_callgraph.py
@@ -0,0 +1,24 @@
+import ubelt as ub
+import networkx as nx
+import parso
+from liberator.core import undefined_names
+
+fpath = ub.Path('~/code/watch/watch/cli/kwcoco_to_geojson.py').expand()
+module = parso.parse(fpath.read_text())
+
+funcs = list(module.iter_funcdefs())
+func_names = {f.name.value for f in funcs}
+
+graph = nx.DiGraph()
+
+for func in funcs:
+    func_name = func.name.value
+    freevars = undefined_names(func.get_code())
+    referenced_freevars = set(freevars) & func_names
+    print(f'referenced_freevars={referenced_freevars}')
+
+    graph.add_node(func_name)
+    for varname in referenced_freevars:
+        graph.add_edge(func_name, varname)
+
+nx.write_network_text(graph)
