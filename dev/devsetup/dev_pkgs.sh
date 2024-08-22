1b83c366d303aa43631cb8e781c7ca217934af42
jon.crall <jon.crall@kitware.com>
2024-08-22 12:25:16 -0400

    update devsetup

--- dev/devsetup/dev_pkgs.sh
+++ dev/devsetup/dev_pkgs.sh
@@ -36,9 +36,29 @@ ndsampler
 simple_dvc
 )
 
+declare -A REPO_REMOTE_LUT=(
+    ["ubelt"]="https://github.com/Erotemic/ubelt.git"
+    ["mkinit"]="https://github.com/Erotemic/mkinit.git"
+    ["xdev"]="https://github.com/Erotemic/xdev.git"
+    ["xdoctest"]="https://github.com/Erotemic/xdoctest.git"
+    ["scriptconfig"]="https://gitlab.kitware.com/utils/scriptconfig.git"
+    ["cmd_queue"]="https://gitlab.kitware.com/computer-vision/cmd_queue.git"
+    ["torch_liberator"]="https://gitlab.kitware.com/computer-vision/torch_liberator.git"
+    ["delayed_image"]="https://gitlab.kitware.com/computer-vision/delayed_image.git"
+    ["kwarray"]="https://gitlab.kitware.com/computer-vision/kwarray.git"
+    ["kwimage"]="https://gitlab.kitware.com/computer-vision/kwimage.git"
+    ["kwplot"]="https://gitlab.kitware.com/computer-vision/kwplot.git"
+    ["kwcoco"]="https://gitlab.kitware.com/computer-vision/kwcoco.git"
+    ["kwutil"]="https://gitlab.kitware.com/computer-vision/kwutil.git"
+    ["kwgis"]="https://gitlab.kitware.com/computer-vision/kwgis.git"
+    ["ndsampler"]="https://gitlab.kitware.com/computer-vision/ndsampler.git"
+    ["simple_dvc"]="https://gitlab.kitware.com/computer-vision/simple_dvc.git"
+)
+
 
 DO_FETCH=1
-DRY_RUN=0
+DO_INSTALL=1
+DO_CLONE=1
 
 
 if [[ "$DO_FETCH" == "1" ]]; then
@@ -46,15 +66,23 @@ if [[ "$DO_FETCH" == "1" ]]; then
     for name in "${mylibs[@]}"
     do
         echo "name = $name"
-        dpath=$CODE_DPATH/$name
-        if [ -d "$dpath" ]; then
+        REPO_DPATH=$CODE_DPATH/$name
+        if [ -d "$REPO_DPATH" ]; then
             #git fetch
-            #(cd "$dpath" && gup)
-            echo "dpath = $dpath"
-            #(cd "$dpath" && git fetch && python ~/local/git_tools/git_devbranch.py update)
-            (cd "$dpath" && git fetch && git-well branch_upgrade)
+            #(cd "$REPO_DPATH" && gup)
+            echo "REPO_DPATH = $REPO_DPATH"
+            #(cd "$REPO_DPATH" && git fetch && python ~/local/git_tools/git_devbranch.py update)
+            (cd "$REPO_DPATH" && git fetch && git-well branch_upgrade)
         else
-            echo "does not exist dpath = $dpath"
+            echo "does not exist REPO_DPATH = $REPO_DPATH"
+            if [[ "$DO_CLONE" == "1" ]]; then
+                # Hack
+                REPO_URL="${REPO_REMOTE_LUT[$name]}"
+                REPO_DPATH=$CODE_DPATH/$name
+                echo "REPO_URL = $REPO_URL"
+                git clone "$REPO_URL" "$REPO_DPATH"
+                (cd "$REPO_DPATH" && git fetch && git-well branch_upgrade)
+            fi
         fi
     done
 fi
@@ -68,21 +96,21 @@ needs_install=()
 for name in "${mylibs[@]}"
 do
     echo "Check: name = $name"
-    dpath=$CODE_DPATH/$name
-    if [[ -d $dpath ]]; then
+    REPO_DPATH=$CODE_DPATH/$name
+    if [[ -d $REPO_DPATH ]]; then
         #base_fpath=$(python -c "import $name; print($name.__file__)")
         if python -c "import sys, $name; sys.exit(1 if 'site-packages' in $name.__file__ else 0)"; then
-            echo " * already have dpath = $dpath"
+            echo " * already have REPO_DPATH = $REPO_DPATH"
         else
-            echo " * will ensure dpath = $dpath"
+            echo " * will ensure REPO_DPATH = $REPO_DPATH"
             needs_uninstall+=("$name")
-            needs_install+=("-e" "$dpath")
+            needs_install+=("-e" "$REPO_DPATH")
             #pip uninstall "$name" -y
             #pip uninstall "$name" -y
-            #pip install -e "$dpath"
+            #pip install -e "$REPO_DPATH"
         fi
     else
-        echo " * does not exist dpath = $dpath"
+        echo " * does not exist REPO_DPATH = $REPO_DPATH"
     fi
 done
 
@@ -97,7 +125,7 @@ bash_array_repr "${needs_install[@]}"
 
 
 
-if [[ "$DRY_RUN" == "0" ]]; then
+if [[ "$DO_INSTALL" == "1" ]]; then
 
     echo "
     Uninstalling:
