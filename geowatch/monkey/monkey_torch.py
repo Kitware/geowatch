672c5f1a8942babfcb89c295bc86084f07025d2a
joncrall <jon.crall@kitware.com>
2025-05-01 20:43:00 -0400

    Update torch monkeypatches and fix issue

--- geowatch/monkey/monkey_torch.py
+++ geowatch/monkey/monkey_torch.py
@@ -1,31 +1,55 @@
 def fix_gelu_issue(method):
-    # Torch 1.12 added an approximate parameter that our old models dont
-    # have. Monkey patch it in.
-    # https://github.com/pytorch/pytorch/pull/61439
+    """
+    Torch 1.12 added an approximate parameter that our old models dont have.
+    Monkey patch it in.
+
+    References:
+        https://github.com/pytorch/pytorch/pull/61439
+    """
     for name, mod in method.named_modules():
-        if mod.__class__.__name__ == 'GELU':
-            if not hasattr(mod, 'approximate'):
-                mod.approximate = 'none'
+        if mod.__class__.__name__ == "GELU":
+            if not hasattr(mod, "approximate"):
+                mod.approximate = "none"
 
 
 def fix_package_modules():
     # Monkey Patch torch.package
     import sys
+
     if sys.version_info[0:2] >= (3, 10):
         try:
             from torch.package import _stdlib
+
             _stdlib._get_stdlib_modules = lambda: sys.stdlib_module_names
         except Exception:
             pass
 
 
 def add_safe_globals():
+    """
+    Stop gap to allow for loading of common classes now that torch load
+    disallows de-serialization of unknown classes.
+
+    References:
+        https://github.com/huggingface/transformers/pull/34632
+    """
     import torch
     import kwcoco
+    import numpy as np
+
     try:
-        context = torch.serialization.safe_globals([kwcoco.category_tree.CategoryTree])
-        context.__enter__()
+        ma = np._core.multiarray
+    except AttributeError:
+        ma = np.core.multiarray
+    try:
+        torch.serialization.add_safe_globals(
+            [
+                kwcoco.category_tree.CategoryTree,
+                ma._reconstruct,
+                np.ndarray,
+                np.dtype,
+                np.dtypes.UInt32DType,
+            ]
+        )
     except AttributeError:
         ...
-    else:
-        assert kwcoco.category_tree.CategoryTree in torch.serialization.get_safe_globals()
